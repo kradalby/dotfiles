4cc96f5b843cd980225beb18df93a42de50b1b0b
Kristoffer Dalby <kradalby@kradalby.no>
2014-05-01 21:04:37 +0200

    added a basic symlink script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CURRENT=`pwd`
+
+function deploy() {
+    ls -1 $CURRENT/rc/ | while read FILE;
+        do
+            rm ~/.$FILE
+            ln -s $CURRENT/rc/$FILE ~/.$FILE
+        done
+}
+
+deploy
