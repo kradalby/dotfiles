54a66c23f0c7c10e24dc07787690ed4c34921809
Kristoffer Dalby <kradalby@kradalby.no>
2014-05-03 10:34:33 +0200

    added vim markdown syntax

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,13 @@ function deploy() {
         done
 }
 
+function install_vim_markdown() {
+    rm -r ~/.vim
+    mkdir ~/.vim
+    wget https://github.com/plasticboy/vim-markdown/archive/master.tar.gz -O ~/.vim/vim_markdown.tar.gz
+    tar --strip=1 -zxf ~/.vim/vim_markdown.tar.gz -C ~/.vim/
+    rm ~/.vim/vim_markdown.tar.gz
+}
+
 deploy
+install_vim_markdown
