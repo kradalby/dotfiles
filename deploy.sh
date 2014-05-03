dc55748a4bf57f40bae53ea021753afd32cf00df
Kristoffer Dalby <kradalby@kradalby.no>
2014-05-03 10:46:14 +0200

    added dhcpd highlight to vim

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,22 @@ function deploy() {
         done
 }
 
-function install_vim_markdown() {
+function prepare_vim_dir() {
     rm -r ~/.vim
     mkdir ~/.vim
+}
+
+function install_vim_markdown() {
     wget https://github.com/plasticboy/vim-markdown/archive/master.tar.gz -O ~/.vim/vim_markdown.tar.gz
     tar --strip=1 -zxf ~/.vim/vim_markdown.tar.gz -C ~/.vim/
     rm ~/.vim/vim_markdown.tar.gz
 }
 
+function install_vim_dhcpd() {
+    wget https://raw.githubusercontent.com/vim-scripts/dhcpd.vim/master/syntax/dhcpd.vim -O ~/.vim/syntax/dhcpd.vim
+}
+
 deploy
+prepare_vim_dir
 install_vim_markdown
+install_vim_dhcpd
