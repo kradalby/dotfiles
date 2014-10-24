9c31f829e8ec4e514378cf5a4dbf9e9e44fcac8e
Kristoffer Dalby <kradalby@kradalby.no>
2014-10-24 09:21:28 +0200

    viiiiim

--- deploy.sh
+++ deploy.sh
@@ -20,30 +20,19 @@ function deploy_special() {
 }
 
 function prepare_vim_dir() {
-    rm -r ~/.vim
+    rm -rf ~/.vim
     mkdir ~/.vim
 }
 
-function install_vim_markdown() {
-    wget https://github.com/plasticboy/vim-markdown/archive/master.tar.gz -O ~/.vim/vim_markdown.tar.gz
-    tar --strip=1 -zxf ~/.vim/vim_markdown.tar.gz -C ~/.vim/
-    rm ~/.vim/vim_markdown.tar.gz
-}
-
-function install_vim_dhcpd() {
-    wget https://raw.githubusercontent.com/vim-scripts/dhcpd.vim/master/syntax/dhcpd.vim -O ~/.vim/syntax/dhcpd.vim
-}
-
-function install_vim_less() {
-    wget "https://github.com/lunaru/vim-less/archive/master.tar.gz" -O ~/.vim/vim_less.tar.gz
-    tar --strip=1 -zxf ~/.vim/vim_less.tar.gz -C ~/.vim/
-    rm ~/.vim/vim_less.tar.gz
+function install_vimplug() {
+    mkdir -p ~/.vim/autoload
+    curl -fLo ~/.vim/autoload/plug.vim \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    vim +PlugInstall
 }
 
 
 deploy
 prepare_vim_dir
-install_vim_markdown
-install_vim_dhcpd
-install_vim_less
+install_vimplug
 deploy_special
