4558ec34dd4b5b14716a1c71ef69bed09e58a532
Kristoffer Dalby <kradalby@kradalby.no>
2015-11-10 09:19:32 +0100

    added screens

--- lnx/config/awesome/rc.lua
+++ lnx/config/awesome/rc.lua
@@ -37,7 +37,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init(".config/awesome/themes/nice-and-clean-theme/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "rxvt"
