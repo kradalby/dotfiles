5eb550924d78246f06e510673593a94be6107293
Kristoffer Dalby <kradalby@kradalby.no>
2016-04-24 23:42:43 +0200

    adds eslint plugins for flow, immutability and errors

--- npm.sh
+++ npm.sh
@@ -10,4 +10,7 @@ eslint \
 eslint-config-standard \
 eslint-plugin-standard \
 eslint-plugin-promise \
-eslint-plugin-react
+eslint-plugin-react \
+eslint-plugin-flowtype \
+eslint-plugin-new-with-error \
+eslint-plugin-immutable
