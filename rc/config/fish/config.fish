4c9ddb01af243cf7171626b5481d346f6da64e74
Kristoffer Dalby <kradalby@kradalby.no>
2016-05-09 22:15:26 +0200

    adds git status chars

--- rc/config/fish/config.fish
+++ rc/config/fish/config.fish
@@ -1,3 +1,22 @@
+# Configure fish
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# # Status Chars
+set __fish_git_prompt_char_dirtystate 'æ'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles 'ø'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
 
 # Homebrew
 set -x PATH ""(brew --prefix)"/bin" $PATH
