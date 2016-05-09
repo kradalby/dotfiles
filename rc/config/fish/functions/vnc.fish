39d2b35c1bcc9ab433d46c317a1bd812abaa2eb4
Kristoffer Dalby <kradalby@kradalby.no>
2016-05-09 11:38:09 +0200

    actually opens VNC

--- rc/config/fish/functions/vnc.fish
+++ rc/config/fish/functions/vnc.fish
@@ -1,10 +1,10 @@
 function vnc -d "Connect to a VNC server"
     if test (count $argv) = 1
         echo "Connecting to: localhost:$argv[1]"
-        #open vnc://localhost:$argv[1]
+        open vnc://localhost:$argv[1]
     else if test (count $argv) = 2
         echo "Connecting to: $argv[1]:$argv[2]"
-        #open vnc://$argv[1]:$argv[2]
+        open vnc://$argv[1]:$argv[2]
     end
 end
 
