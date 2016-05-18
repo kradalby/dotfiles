675bd88ecaf5ec73166e535e31ccc303dddd392b
Kristoffer Dalby <kradalby@kradalby.no>
2016-05-18 13:34:07 +0200

    adds opam init to fish

--- rc/config/fish/config.fish
+++ rc/config/fish/config.fish
@@ -87,3 +87,6 @@ alias gbd 'git branch -d '
 alias gp 'git pull'
 alias gss 'git status -s'
 alias gst 'git stash'
+
+# OPAM configuration
+. $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null or true
