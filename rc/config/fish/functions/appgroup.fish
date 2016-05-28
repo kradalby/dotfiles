58c726b1734e88715ec8ffdddbbab5a1a5aad487
Kristoffer Dalby <kradalby@kradalby.no>
2016-05-28 18:20:24 +0200

    adds appgroup function to kill and start groups of apps

---
+++ rc/config/fish/functions/appgroup.fish
@@ -0,0 +1,19 @@
+function appgroup
+    set comm Slack Discord Goofy Skype ChitChat
+
+    set base Safari iTerm Spotify Mail Stickies $comm
+
+    set group $$argv[1]
+
+    if test $argv[2] = "start"
+        for app in $group
+            echo "Starting $app"
+            open -a $app
+        end
+    else if test $argv[2] = "kill"
+        for app in $group
+            echo "Killing $app"
+            osascript -e "quit app \"$app\""
+        end
+    end
+end
