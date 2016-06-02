7aaf4d551b17feb99b7f9430cca7889afcf726a2
Kristoffer Dalby <kradalby@kradalby.no>
2016-06-02 20:20:36 +0200

    changes bright colors to hex as they are not in 2.2

--- rcconfig/fish/functions/fish_prompt.fish
+++ rcconfig/fish/functions/fish_prompt.fish
@@ -1,18 +1,21 @@
 function fish_prompt
+    set brmagenta FF55FF
+    set bryellow FFFF55
+    set brgreen 55FF55
     # kradalby@kramacbook ~/g/dotfiles (master=)
     echo
-    set_color brmagenta
+    set_color $brmagenta
     date "+%m/%d/%y %H:%M:%S"
-    set_color bryellow
+    set_color $bryellow
     printf $USER
     set_color green
     printf "@"
-    set_color brgreen
+    set_color $brgreen
     printf (hostname)
-    set_color brmagenta
+    set_color $brmagenta
     printf ' %s' (prompt_pwd)
     set_color normal
     printf '%s' (__fish_git_prompt)
-    set_color brmagenta
+    set_color $brmagenta
     echo " -> "
 end
