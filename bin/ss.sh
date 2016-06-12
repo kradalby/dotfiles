e10ee8c8a88efc974d37806cb4c8b7e5c9719088
Kristoffer Dalby <kradalby@kradalby.no>
2016-06-12 17:16:40 +0200

    fixes ss.sh, moves from onyx to seel

--- bin/ss.sh
+++ bin/ss.sh
@@ -2,7 +2,7 @@
 
 # Use this as a osx service.
 # Edit for own needs.
-# 
+#
 # Install:
 # Open Automater.app
 # Create Service
@@ -15,5 +15,5 @@ filename=`date '+%Y%m%d%H%M%S'`.png
 path=~/Pictures/ss/
 mkdir -p $path
 screencapture -o -i $path$filename
-scp -v $path$filename onyx:/usr/local/www/kradalby.no/ss/
+scp -v $path$filename seel:/srv/www/kradalby.no/ss/
 printf $url$filename | pbcopy
