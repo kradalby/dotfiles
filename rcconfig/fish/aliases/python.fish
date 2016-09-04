751390aa90be1f50b22eff662af784b4acdd6e5d
Kristoffer Dalby <kradalby@kradalby.no>
2016-09-04 17:50:39 +0200

    adds alias for ipython instead of python if installed

--- rcconfig/fish/aliases/python.fish
+++ rcconfig/fish/aliases/python.fish
@@ -1,7 +1,15 @@
-alias python ipython
-alias python3 ipython3
-alias py python
-alias py3 python3
+
+if test -e (which ipython)
+    alias python ipython
+    alias python3 ipython3
+    alias py ipython
+    alias py3 ipython3
+    alias pyt (which python)
+    alias pyt3 (which python3)
+else
+    alias py python
+    alias py3 python3
+end
 
 alias pp pypy
 alias pp3 pypy3
