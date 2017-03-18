9e135c678987c32509cd422705c08594dbeb7cc9
Kristoffer Dalby <kradalby@kradalby.no>
2017-03-18 19:09:18 +0100

    refines pictures command

--- rcconfig/fish/functions/pictures.fish
+++ rcconfig/fish/functions/pictures.fish
@@ -1,14 +1,42 @@
 function pictures
+    set DIR (string join "" $TMPDIR pictures)
+    set PDIR (string join "" "/private" $DIR)
+
     switch $argv[1]
         case "mount"
-            mkdir $TMPDIR/pictures/
-            sshfs seel:/storage/pictures $TMPDIR/pictures/
-            echo "Mounted at $TMPDIR/pictures"
+            if mount | grep "on $PDIR"
+                echo "$DIR already mounted"
+            else
+                echo "$DIR is not mounted"
+                mkdir -p $DIR
+                sshfs seel:/storage/pictures $DIR
+                echo "Mounted at $DIR"
+            end
+        case "umount"
+            if mount | grep "on $PDIR"
+                umount $DIR
+            else
+                echo "$DIR is not mounted"
+            end
         case "cd"
-            cd $TMPDIR/pictures
+            if mount | grep "on $PDIR"
+                cd $DIR
+            else
+                echo "Pictures not mounted, run: pictures mount"
+            end
         case "open"
-            open $TMPDIR/pictures
+            if mount | grep "on $PDIR"
+                open $DIR
+            else
+                echo "Pictures not mounted, run: pictures mount"
+            end
         case "ls"
-            ls $TMPDIR/pictures
+            if mount | grep "on $PDIR"
+                ls $DIR
+            else
+                echo "Pictures not mounted, run: pictures mount"
+            end
+        case "*"
+            echo "Usage: pictures (mount|cd|open|ls)"
         end
 end
