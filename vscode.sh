4f911ccbf3dfa38a078d56c6424ef9cf2f442e70
Kristoffer Dalby <kradalby@kradalby.no>
2017-09-26 19:04:40 +0200

    adds new vscode extensions

--- vscode.sh
+++ vscode.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/env bash
 
 extensions="Ionide.Ionide-fsharp
+James-Yu.latex-workshop
 PeterJausovec.vscode-docker
+Zignd.html-css-class-completion
 abusaidm.html-snippets
+be5invis.toml
 bibhasdn.django-html
+christian-kohler.path-intellisense
 dbaeumer.vscode-eslint
 donjayamanne.javadebugger
 donjayamanne.python
 eg2.tslint
 eg2.vscode-npm-script
+freebroccolo.reasonml
 haaaad.ansible
-hackwaly.ocaml
 mrmlnc.vscode-stylefmt
-ms-vscode.PowerShell
 ms-vscode.cpptools
 ms-vscode.csharp
+ms-vscode.PowerShell
 msjsdiag.debugger-for-chrome
 rbbit.typescript-hero
 robertohuertasm.vscode-icons
-sbrink.elm
-seanmcbreen.Spell
-akmittal.hugofy
 saviorisdead.RustyCode
+sbrink.elm
 shinnn.stylelint"
 
 for ext in $extensions
