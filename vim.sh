f65b99baa297f0e05373dd48c16f4b2bec07a1d1
Kristoffer Dalby <kradalby@kradalby.no>
2018-02-06 12:16:37 +0100

    vim: ale column always, remove invis chars, shfmt

--- vim.sh
+++ vim.sh
@@ -10,6 +10,9 @@ npm install -g elm-format
 # HCL
 go get -u github.com/fatih/hclfmt
 
+# Shell
+go get -u mvdan.cc/sh/cmd/shfmt
+
 # deoplete.nvim
 pip2 install --upgrade neovim
 pip3 install --upgrade neovim
