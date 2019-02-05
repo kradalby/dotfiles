3a016924f1b9885ce58a6a0fafd8fa212ac06c6d
Kristoffer Dalby <kradalby@kradalby.no>
2019-02-05 08:18:36 +0100

    more kubectl aliases

--- rcconfig/fish/aliases/kubectl.fish
+++ rcconfig/fish/aliases/kubectl.fish
@@ -6,3 +6,4 @@ alias ke 'kubectl edit'
 alias kaf 'kubectl apply -f'
 alias kdf 'kubectl delete -f'
 alias kshell 'kubectl run --generator=run-pod/v1 ubuntu-shell --rm -i --tty --image ubuntu -- bash'
+
---
+++ rcconfig/fish/functions/ks.fish
@@ -0,0 +1,3 @@
+function ks
+    set -xg KUBECONFIG "$argv[1]"
+end
