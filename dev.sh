ecb571c71094b8f9f8db1140ce4fcdf7e643c0c0
Kristoffer Dalby <kradalby@kradalby.no>
2019-02-06 14:30:59 +0100

    adds more gotools

--- dev.sh
+++ dev.sh
@@ -15,13 +15,19 @@ eslint \
 git-open \
 vscode-css-languageserver-bin \
 yaml-language-server \
-javascript-typescript-langserver 
+javascript-typescript-langserver
 
 ####################
 # GO
 ####################
 set -x GO111MODULE off
 
+# Go buffalo
+go get -u -v github.com/gobuffalo/buffalo/buffalo
+go get github.com/gobuffalo/pop/...
+go install github.com/gobuffalo/pop/soda
+
+# Go Language Server
 go install github.com/saibing/bingo
 
 set -x GO111MODULE on
