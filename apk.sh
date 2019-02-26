8707cce0691021b5ee1eea21d8e6680536733d3f
Kristoffer Dalby <kradalby@kradalby.no>
2019-02-26 04:32:34 +0100

    adds apk file for iphone iSH

---
+++ apk.sh
@@ -0,0 +1,6 @@
+# Packages needed on iSH on iPhone
+
+# Start ssh server for easier access
+# /usr/sbin/sshd -D
+
+apk add vim git python openssh python3 fish bash curl fzf the_silver_searcher py3-pip
