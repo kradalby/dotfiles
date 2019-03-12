00d52deb8bacdf0a78b00d268600ceff3c797dc2
Kristoffer Dalby <kradalby@kradalby.no>
2019-03-12 14:30:10 +0100

    moves nvim to rcc instead of rc

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,6 @@ function deploy() {
             ln -s $CURRENT/rc/$FILE ~/.$FILE
         done
     echo "Linking vimrc to neovim"
-    mkdir -p $HOME/.config/nvim
-    ln -s $CURRENT/rc/vimrc $HOME/.config/nvim/init.vim
 }
 
 function deploy_ssh() {
