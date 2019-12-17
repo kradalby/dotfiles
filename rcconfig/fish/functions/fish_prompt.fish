b59426a23491d79015663501c457694169317395
Kristoffer Dalby <kradalby@kradalby.no>
2019-12-17 21:35:54 +0000

    breaks fish prompt into one more line, do full pwd

--- rcconfig/fish/functions/fish_prompt.fish
+++ rcconfig/fish/functions/fish_prompt.fish
@@ -1,21 +1,24 @@
 function fish_prompt
+	set d (date "+%m/%d/%y %H:%M:%S")
     set brmagenta FF55FF
     set bryellow FFFF55
     set brgreen 55FF55
     # kradalby@kramacbook ~/g/dotfiles (master=)
     echo
     set_color $brmagenta
-    date "+%m/%d/%y %H:%M:%S"
+    printf '%s ' $d
     set_color $bryellow
     printf $USER
     set_color green
     printf "@"
     set_color $brgreen
     printf (hostname)
-    set_color $brmagenta
-    printf ' %s' (prompt_pwd)
+    set_color brcyan
+    echo
+    printf '%s' (pwd)
     set_color normal
     printf '%s' (__fish_git_prompt)
     set_color $brmagenta
-    echo " -> "
+    echo
+    echo "> "
 end
