a20188fcad24a34ea5d9686c147b7e6534257c9b
Kristoffer Dalby <kradalby@kradalby.no>
2020-01-28 18:46:19 +0000

    Add awscli to pipx

--- pipx.fish
+++ pipx.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
-for pkg in ansible gitlint tox black flake8 pylint pyre-check autopep8
+for pkg in ansible gitlint tox black flake8 pylint pyre-check autopep8 awscli
     pipx install $pkg
 end
