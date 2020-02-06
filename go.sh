a13e08642fe14fbef9d7d59776e592ed301057aa
Kristoffer Dalby <kradalby@kradalby.no>
2020-02-06 10:47:39 +0000

    Add cue and kubespace to go

--- go.sh
+++ go.sh
@@ -4,3 +4,6 @@ go get -u golang.org/x/tools/gopls@latest
 go get -u github.com/mitchellh/gox
 go get -u mvdan.cc/sh/cmd/shfmt
 go get -u github.com/fatih/hclfmt
+go get -u github.com/kradalby/kubespace
+go get -u cuelang.org/go/cmd/cue
+
