14f886fa16dcfc7fc142eea82836cb60b06acb62
Kristoffer Dalby <kradalby@kradalby.no>
2020-02-16 21:12:32 +0000

    Add alias to check sms

--- rcconfig/fish/aliases/general.fish
+++ rcconfig/fish/aliases/general.fish
@@ -11,3 +11,5 @@ alias ed25519key 'ssh-keygen -t ed25519 -o -a 100'
 alias reload-fish 'source $HOME/.config/fish/config.fish'
 
 alias simulator 'open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app'
+
+alias getsms 'curl https://api.smstore.kradalby.no/sms -s | jq --raw-output \'.[] | "\(.sender): \(.message)"\''
