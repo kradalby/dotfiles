87cbfa35b516855b4353821305f74dc671932c7d
Kristoffer Dalby <kradalby@kradalby.no>
2020-02-28 18:17:36 +0000

    Add vscode tools to go.sh

--- go.sh
+++ go.sh
@@ -8,3 +8,27 @@ go get -u github.com/kradalby/kubespace
 go get -u cuelang.org/go/cmd/cue
 go get -u github.com/prometheus-community/promql-langserver/cmd/promql-langserver
 
+# Tools for vscode-go
+go get -u golang.org/x/tools/gopls
+go get -u golang.org/x/tools/cmd/goimports
+go get -u github.com/godoctor/godoctor
+go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
+go get -u honnef.co/go/tools/...
+go get -u golang.org/x/tools/cmd/guru
+go get -u golang.org/x/tools/cmd/gorename
+go get -u github.com/fatih/gomodifytags
+go get -u github.com/haya14busa/goplay/cmd/goplay
+go get -u github.com/josharian/impl
+go get -u github.com/tylerb/gotype-live
+go get -u github.com/rogpeppe/godef
+go get -u github.com/zmb3/gogetdoc
+go get -u github.com/sqs/goreturns
+go get -u winterdrache.de/goformat/goformat
+go get -u golang.org/x/lint/golint
+go get -u github.com/cweill/gotests/...
+go get -u github.com/mgechev/revive
+go get -u github.com/go-delve/delve/cmd/dlv
+go get -u github.com/davidrjenni/reftools/cmd/fillstruct
+go get -u github.com/uudashr/gopkgs/cmd/gopkgs
+go get -u github.com/ramya-rao-a/go-outline
+go get -u github.com/acroca/go-symbols
