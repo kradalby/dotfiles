75ea9b8f1a80dda9b17d3f78b515baa06eca9e4e
Kristoffer Dalby <kradalby@kradalby.no>
2020-06-21 07:47:06 +0100

    Test trigger drone

--- rc/hyper.js
+++ rc/hyper.js
@@ -136,7 +136,9 @@ module.exports = {
 
     // Whether to use the WebGL renderer. Set it to false to use canvas-based
     // rendering (slower, but supports transparent backgrounds)
-    webGLRenderer: true
+    webGLRenderer: true,
+
+    webLinksActivationKey: 'meta'
 
     // for advanced config flags please refer to https://hyper.is/#cfg
   },
@@ -147,7 +149,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-quit', 'hypercwd'],
+  plugins: ['hyper-quit'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
