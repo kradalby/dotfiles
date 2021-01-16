36e010b850ff4dda5bafac2aaa5a3e15edcb9248
Kristoffer Dalby <kradalby@kradalby.no>
2021-01-16 16:16:08 +0000

    Comment out some rarely used pipx

--- pipx.sh
+++ pipx.sh
@@ -5,11 +5,11 @@ pipx install poetry
 pipx install ansible-base
 pipx install ansible-lint
 pipx inject pipx ansible
-pipx install gitlint
-pipx install tox
+# pipx install gitlint
+# pipx install tox
 pipx install black
-pipx install flake8
+# pipx install flake8
 pipx install pylint
 pipx install pyre-check
-pipx install autopep8
-pipx install awscli
+# pipx install autopep8
+# :pipx install awscli
