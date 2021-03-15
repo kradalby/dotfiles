199125dc2b7646cd3252954dd6736a2eb51906b5
Kristoffer Dalby <kradalby@kradalby.no>
2021-03-15 09:24:34 +0000

    Stuff

--- rcconfig/fish/aliases/terraform.fish
+++ rcconfig/fish/aliases/terraform.fish
@@ -1,5 +1,6 @@
 alias tf  "terraform"
 alias tfi "terraform init"
+alias tfiu "terraform init -upgrade"
 alias tfp "terraform plan"
 alias tfa "terraform apply"
 alias tfaaa "terraform apply -auto-approve"
