a79600ba7d87d0bdd33cd2be7ecd8a4ba18d1c57
Kristoffer Dalby <kradalby@kradalby.no>
2021-06-04 16:48:40 +0100

    Updates

--- rcconfig/nvim/lua/plugins.lua
+++ rcconfig/nvim/lua/plugins.lua
@@ -14,11 +14,15 @@ vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
 return require("packer").startup(
     function(use)
         use 'wbthomason/packer.nvim'
+
         use "nvim-treesitter/nvim-treesitter"
+        use "p00f/nvim-ts-rainbow"
+
         use "neovim/nvim-lspconfig"
         use "kabouzeid/nvim-lspinstall"
         use "hrsh7th/nvim-compe"
         use "ray-x/lsp_signature.nvim"
+
         use "dense-analysis/ale" -- TODO: Replace with Lua based plugin
         use "tanvirtin/monokai.nvim"
         use "nvim-lua/popup.nvim"
