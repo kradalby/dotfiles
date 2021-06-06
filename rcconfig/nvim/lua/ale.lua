690a18094d007495e3f09b8ba386c541c9b2d495
Kristoffer Dalby <kradalby@kradalby.no>
2021-06-06 14:44:09 +0100

    nvim code splut

---
+++ rcconfig/nvim/lua/ale.lua
@@ -0,0 +1,36 @@
+local g = vim.g
+g.ale_linters = {
+    go = {"golangci-lint"},
+    terraform = {"terraform", "tflint"},
+    python = {"pylint", "flake8", "pyre", "mypy", "pyright"},
+    ansible = {"ansible-lint"},
+    dockerfile = {"dockerfile_lint", "hadolint"},
+    swift = {"apple-swift-format"},
+    fish = {"fish_indent"}
+}
+
+g.ale_fixers = {
+    javascript = {"prettier", "eslint"},
+    typescript = {"prettier", "eslint"},
+    elm = {"format"},
+    sh = {"shfmt"},
+    go = {"goimports"},
+    terraform = {"terraform"},
+    html = {"prettier"},
+    css = {"prettier"},
+    scss = {"prettier"},
+    json = {"prettier"},
+    yaml = {"prettier"},
+    fsharp = {"fantomas"},
+    python = {"autoimport", "isort", "black"},
+    lua = {"luafmt", "black"},
+    swift = {"apple-swift-format"}
+}
+g.ale_fixers["*"] = {"remove_trailing_lines", "trim_whitespace"}
+
+g.ale_fix_on_save = 1
+g.ale_lint_on_save = 1
+g.ale_completion_enabled = 0
+g.ale_sign_column_always = 1
+g.ale_linters_explicit = 0
+g.ale_python_flake8_options = "--max-line-length=88"
--- rcconfig/nvim/lua/plugins.lua
+++ rcconfig/nvim/lua/plugins.lua
@@ -1,32 +1,103 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+    fn.system({"git", "clone", "https://github.com/wbthomason/packer.nvim", install_path})
+    execute "packadd packer.nvim"
 end
 
 vim.cmd [[packadd packer.nvim]]
-vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
+vim.cmd "autocmd BufWritePost plugins.lua PackerCompile"
 
 return require("packer").startup(
     function(use)
-        use 'wbthomason/packer.nvim'
+        use "wbthomason/packer.nvim"
 
-        use "nvim-treesitter/nvim-treesitter"
-        use "p00f/nvim-ts-rainbow"
+        use {
+            "nvim-treesitter/nvim-treesitter",
+            run = ":TSUpdate",
+            requires = {"p00f/nvim-ts-rainbow"},
+            config = function()
+                require "nvim-treesitter.configs".setup {
+                    ensure_installed = "all",
+                    ignore_install = {"haskell"}, -- Haskell breaks without Java?
+                    highlight = {enable = true},
+                    rainbow = {
+                        enable = true,
+                        extended_mode = true,
+                        max_file_lines = 1000
+                    }
+                }
+            end
+        }
 
-        use "neovim/nvim-lspconfig"
-        use "kabouzeid/nvim-lspinstall"
-        use "hrsh7th/nvim-compe"
-        use "ray-x/lsp_signature.nvim"
+        use {
+            "norcalli/nvim-colorizer.lua",
+            config = function()
+                require "colorizer".setup()
+            end
+        }
 
-        use "dense-analysis/ale" -- TODO: Replace with Lua based plugin
+        use {
+            "neovim/nvim-lspconfig",
+            requires = {
+                "nvim-lua/lsp-status.nvim",
+                -- "kabouzeid/nvim-lspinstall",
+                "kradalby/nvim-lspinstall" -- TODO: remove when #83 is resolved
+            },
+        }
+
+        use {
+            "hrsh7th/nvim-compe",
+            config = function()
+                require "compe".setup {
+                    source = {
+                        path = true,
+                        buffer = true,
+                        calc = true,
+                        nvim_lsp = true,
+                        nvim_lua = true,
+                        treesitter = true
+                    }
+                }
+            end
+        }
+        use {
+            "ray-x/lsp_signature.nvim",
+            config = function()
+                require "lsp_signature".on_attach()
+            end
+        }
+
+        use {
+            "dense-analysis/ale",
+            config = function()
+                require("ale")
+            end
+            -- TODO: Only certain files?
+        } -- TODO: Replace with Lua based plugin
+
         use "tanvirtin/monokai.nvim"
         use "nvim-lua/popup.nvim"
         use "nvim-lua/plenary.nvim"
         use "nvim-telescope/telescope.nvim"
+
+        -- Filetypes
+        use "sheerun/vim-polyglot"
+
+        -- Github integration
+        if vim.fn.executable "gh" == 1 then
+            use "pwntester/octo.nvim"
+        end
+
+        use {
+            "folke/trouble.nvim",
+            requires = "kyazdani42/nvim-web-devicons",
+            config = function()
+                require("trouble").setup {}
+            end
+        }
     end
 )
