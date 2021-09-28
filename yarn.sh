8f9806309e4ffd987fdc82557865d592aa6bd27e
Kristoffer Dalby <kradalby@kradalby.no>
2021-09-28 16:22:23 +0100

    Add xml support to prettier

--- yarn.sh
+++ yarn.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env sh
 
 yarn global add \
-    typescript \
-    diff-so-fancy \
-    git-split-diffs \
-    vscode-css-languageserver-bin \
-    yaml-language-server \
-    dockerfile-language-server-nodejs \
-    ocaml-language-server \
-    bash-language-server \
-    git-open \
-    elm-json \
-    elm-test \
-    @elm-tooling/elm-language-server \
-    bash-language-server \
-    ansible-language-server \
-    tailwindcss \
-    browser-sync \
-    lua-fmt \
-    zx \
-    pyright \
-    prettier
+	typescript \
+	diff-so-fancy \
+	git-split-diffs \
+	vscode-css-languageserver-bin \
+	yaml-language-server \
+	dockerfile-language-server-nodejs \
+	ocaml-language-server \
+	bash-language-server \
+	git-open \
+	elm-json \
+	elm-test \
+	@elm-tooling/elm-language-server \
+	bash-language-server \
+	ansible-language-server \
+	tailwindcss \
+	browser-sync \
+	lua-fmt \
+	zx \
+	pyright \
+	prettier \
+	@prettier/plugin-xml
