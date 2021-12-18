697b379c9eca7e34ef2fb979eec8e31da150ff29
Kristoffer Dalby <kradalby@kradalby.no>
2021-12-18 15:46:11 +0000

    Add flakes of the future

--- flake.nix
+++ flake.nix
@@ -2,21 +2,35 @@
   description = "kradalby's system config";
 
   inputs = {
-    nixos-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixos-old.url = github:NixOS/nixpkgs/nixos-21.05;
+    nixos.url = github:NixOS/nixpkgs/nixos-21.11;
+    nixos-unstable.url = github:nixos/nixpkgs/nixpkgs-unstable;
 
-    darwin.url = "github:lnl7/nix-darwin/master";
+    nixos-hardware.url = github:NixOS/nixos-hardware;
+
+    darwin.url = github:lnl7/nix-darwin/master;
     darwin.inputs.nixpkgs.follows = "nixos-unstable";
 
-    home-manager-unstable.url = "github:nix-community/home-manager/master";
+    home-manager-old.url = github:nix-community/home-manager/release-21.05;
+    home-manager.url = github:nix-community/home-manager/release-21.11;
+    home-manager-unstable.url = github:nix-community/home-manager/master;
     home-manager-unstable.inputs.nixpkgs.follows = "nixos-unstable";
+
+    nur.url = github:nix-community/NUR;
   };
 
   outputs =
     { self
+    , nixos-old
+    , nixos
     , nixos-unstable
     , darwin
+    , home-manager-old
+    , home-manager
     , home-manager-unstable
-    }: {
+    , nur
+    , ...
+    } @ flakes: {
       darwinConfigurations.kramacbook =
         let
           system = "x86_64-darwin";
