96ff8dca2a837a642afaad1c93bdb3a07c8167f8
Kristoffer Dalby <kradalby@kradalby.no>
2021-12-26 21:30:02 +0100

    changes

--- machines/dev.terra/hardware-configuration.nix
+++ machines/dev.terra/hardware-configuration.nix
@@ -1,6 +1,9 @@
 { modulesPath, ... }:
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
   boot.loader.grub.devices = [ "/dev/vda" ];
   boot.initrd.kernelModules = [ "nvme" ];
   fileSystems."/" = {
--- machines/kramacbook/default.nix
+++ machines/kramacbook/default.nix
@@ -6,7 +6,7 @@
 {
 
   # sops.age.sshKeyPaths = [ "~/.ssh/id_ed25519" ];
-  sops.age.keyFile = "~/.config/sops/age/keys.txt";
+  # sops.age.keyFile = "~/.config/sops/age/keys.txt";
 
   services.nix-daemon = {
     enable = true;
