79b2cb2280bfd888eddd5143682ee23b334d7285
Kristoffer Dalby <kradalby@kradalby.no>
2022-01-19 21:48:57 +0000

    switch to timesyncd

--- common/time.nix
+++ common/time.nix
@@ -1,6 +1,6 @@
 {
   services.chrony = {
-    enable = true;
+    enable = false;
     servers = [
       "time1.google.com"
       "time2.google.com"
@@ -19,4 +19,25 @@
   };
 
   systemd.services.chrony.onFailure = [ "notify-discord@%n.service" ];
+
+  services.timesyncd = {
+    enable = true;
+    servers = [
+      "time1.google.com"
+      "time2.google.com"
+      "time3.google.com"
+      "time4.google.com"
+
+      # 0.uk.pool.ntp.org
+      "143.210.16.201"
+      "178.79.160.57"
+      "217.114.59.3"
+      "87.117.251.3"
+    ];
+    extraConfig = ''
+      rtcsync
+    '';
+  };
+
+  systemd.services.timesyncd.onFailure = [ "notify-discord@%n.service" ];
 }
