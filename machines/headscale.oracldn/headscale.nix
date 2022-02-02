1b260fbbfabbae1df9b5e4e5784c9dfec1ed4504
Kristoffer Dalby <kradalby@kradalby.no>
2022-02-02 22:09:34 +0000

    Use headscale without nginx

--- machines/headscale.oracldn/headscale.nix
+++ machines/headscale.oracldn/headscale.nix
@@ -24,7 +24,14 @@ in
     # package = pkgs.unstable.headscale;
 
     address = "0.0.0.0";
+    port = 443;
     serverUrl = "https://${domain}";
+
+    tls = {
+      letsencrypt = {
+        hostname = domain;
+      };
+    };
 
     privateKeyFile = config.age.secrets.headscale-private-key.path;
 
@@ -50,35 +57,36 @@ in
 
   my.consulServices.headscale = consul.prometheusExporter "headscale" config.services.headscale.port;
 
-  security.acme.certs."${domain}".domain = domain;
-
-  services.nginx.virtualHosts."${domain}" = {
-    forceSSL = true;
-    useACMEHost = domain;
-    locations."/metrics" = {
-      proxyPass = "http://127.0.0.1:${toString config.services.headscale.port}";
-      extraConfig = ''
-        allow 10.0.0.0/8;
-        allow 100.64.0.0/16;
-        deny all;
-      '';
-    };
-    locations."/" = {
-      proxyPass = "http://127.0.0.1:${toString config.services.headscale.port}";
-      proxyWebsockets = true;
-      extraConfig = ''
-        keepalive_requests          100000;
-        keepalive_timeout           160s;
-        proxy_buffering             off;
-        proxy_connect_timeout       75;
-        proxy_ignore_client_abort   on; 
-        proxy_read_timeout          900s;
-        proxy_send_timeout          600;
-        send_timeout                600;
-      '';
-    };
-    extraConfig = ''
-      access_log /var/log/nginx/${domain}.access.log;
-    '';
-  };
+  # security.acme.certs."${domain}".domain = domain;
+  #
+  # services.nginx.virtualHosts."${domain}" = {
+  #   forceSSL = true;
+  #   useACMEHost = domain;
+  #   locations."/metrics" = {
+  #     proxyPass = "http://127.0.0.1:${toString config.services.headscale.port}";
+  #     extraConfig = ''
+  #       allow 10.0.0.0/8;
+  #       allow 100.64.0.0/16;
+  #       deny all;
+  #     '';
+  #   };
+  #   locations."/" = {
+  #     proxyPass = "http://127.0.0.1:${toString config.services.headscale.port}";
+  #     proxyWebsockets = true;
+  #     extraConfig = ''
+  #       grpc_pass grpc://127.0.0.1:${toString config.services.headscale.port};
+  #       keepalive_requests          100000;
+  #       keepalive_timeout           160s;
+  #       proxy_buffering             off;
+  #       proxy_connect_timeout       75;
+  #       proxy_ignore_client_abort   on; 
+  #       proxy_read_timeout          900s;
+  #       proxy_send_timeout          600;
+  #       send_timeout                600;
+  #     '';
+  #   };
+  #   extraConfig = ''
+  #     access_log /var/log/nginx/${domain}.access.log;
+  #   '';
+  # };
 }
