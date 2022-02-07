a650c6267182ce08fc0fa2aa33d6347dfe7c44de
Kristoffer Dalby <kradalby@kradalby.no>
2022-02-07 22:19:09 +0000

    working k3s

--- common/funcs/k3s.nix
+++ common/funcs/k3s.nix
@@ -6,9 +6,27 @@ let
 
       environment.systemPackages = [
         (pkgs.writeShellScriptBin "k3s-reset-node" (builtins.readFile ./k3s-reset-node))
+        pkgs.nfs-utils
       ];
 
-      networking.firewall.allowedTCPPorts = [ 6443 ];
+      networking.firewall.allowedTCPPorts = [
+        6443 # API Server
+        10250 # Kubelet API
+        7946 # MetalLB
+        179 # BGP / Bird
+        80 # HTTP
+        443 # HTTPS
+      ];
+      networking.firewall.allowedUDPPorts = [
+        8472 # Flannel VXLAN
+        7946 # MetalLB
+      ];
+      # networking.firewall.logRefusedPackets = true;
+
+      # Workaround for mount rpc issue
+      # https://github.com/NixOS/nixpkgs/issues/76671
+      services.nfs.server.enable = true;
+      services.rpcbind.enable = true;
 
       services.k3s = {
         enable = true;
@@ -18,10 +36,10 @@ let
           "--disable traefik"
           "--disable servicelb"
           "--disable local-storage"
-          "--flannel-backend=none"
-          "--disable-network-policy"
-          "--cluster-cidr ${site.k3s.clusterCidr}"
-          "--service-cidr ${site.k3s.serviceCidr}"
+          # "--flannel-backend=none"
+          # "--disable-network-policy"
+          # "--cluster-cidr ${site.k3s.clusterCidr}"
+          # "--service-cidr ${site.k3s.serviceCidr}"
         ];
 
         tokenFile = config.age.secrets."k3s-${site.name}".path;
@@ -34,7 +52,25 @@ let
 
       environment.systemPackages = [
         (pkgs.writeShellScriptBin "k3s-reset-node" (builtins.readFile ./k3s-reset-node))
+        pkgs.nfs-utils
       ];
+
+      networking.firewall.allowedTCPPorts = [
+        10250 # Kubelet API
+        7946 # MetalLB
+        179 # BGP / Bird
+        80 # HTTP
+        443 # HTTPS
+      ];
+      networking.firewall.allowedUDPPorts = [
+        8472 # Flannel VXLAN
+        7946 # MetalLB
+      ];
+
+      # Workaround for mount rpc issue
+      # https://github.com/NixOS/nixpkgs/issues/76671
+      services.nfs.server.enable = true;
+      services.rpcbind.enable = true;
 
       services.k3s = {
         enable = true;
