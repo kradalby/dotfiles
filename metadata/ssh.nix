615fbafa04cb0d0cbe1a5fe6475cf8f2eeb1a496
Kristoffer Dalby <kradalby@kradalby.no>
2022-06-11 20:51:24 +0100

    Manage auth keys for macbook pro with nix

---
+++ metadata/ssh.nix
@@ -0,0 +1,13 @@
+{
+  main = [
+    # kramacbook
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBV4ZjlUvRDs70qHD/Ldi6OTkFpDEFgfbXbqSnaL2Qup"
+
+    # dev vm
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGyrauaLwnrgeR5mpeOBCw/creVh1dMU1a12TTXvQ+Rd"
+  ];
+
+  kradalby = [
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFiqc4Mou7XVbFNEY0EDkD34G1JbtuFB0WndjktfiBBxS3hz0XdE/mCUjS5Zs65mg5aKzdQXisGRX85LT4DTAtQ= kphone@blink"
+  ];
+}
