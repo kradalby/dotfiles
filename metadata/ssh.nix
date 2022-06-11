b43ac354c3453fd06242863f14e76e51609e7eec
Kristoffer Dalby <kradalby@kradalby.no>
2022-06-11 20:53:49 +0100

    Move comments for ssh keys

--- metadata/ssh.nix
+++ metadata/ssh.nix
@@ -3,11 +3,12 @@
     # kramacbook
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBV4ZjlUvRDs70qHD/Ldi6OTkFpDEFgfbXbqSnaL2Qup"
 
-    # dev vm
+    # dev.ntnu vm
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGyrauaLwnrgeR5mpeOBCw/creVh1dMU1a12TTXvQ+Rd"
   ];
 
   kradalby = [
-    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFiqc4Mou7XVbFNEY0EDkD34G1JbtuFB0WndjktfiBBxS3hz0XdE/mCUjS5Zs65mg5aKzdQXisGRX85LT4DTAtQ= kphone@blink"
+    # kPhone
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFiqc4Mou7XVbFNEY0EDkD34G1JbtuFB0WndjktfiBBxS3hz0XdE/mCUjS5Zs65mg5aKzdQXisGRX85LT4DTAtQ="
   ];
 }
