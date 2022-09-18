1c0ab54c22476dc90e3d5c89a9ca24a9b4c6ad21
Kristoffer Dalby <kristoffer@dalby.cc>
2022-09-18 12:08:26 +0200

    Add consul for tjoda to prom
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/core.oracldn/monitoring.nix
+++ machines/core.oracldn/monitoring.nix
@@ -7,12 +7,15 @@ in
   services.prometheus = {
     enable = true;
 
+    retentionTime = "365d";
+
     scrapeConfigs = [
       {
         job_name = "consul";
         consul_sd_configs = [
           { server = "consul.ldn.fap.no"; }
           { server = "consul.ntnu.fap.no"; }
+          { server = "consul.tjoda.fap.no"; }
         ];
         relabel_configs = [
           {
