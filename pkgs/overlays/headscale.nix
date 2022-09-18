39e00f49ed60d195852d73b865451e557e8eb7de
Kristoffer Dalby <kristoffer@dalby.cc>
2022-09-18 12:05:50 +0200

    Add noise key to headscale
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- pkgs/overlays/headscale.nix
+++ pkgs/overlays/headscale.nix
@@ -5,15 +5,15 @@ buildGoModule rec {
   version = "0.17.0";
 
   src = fetchFromGitHub {
-    owner = "juanfont";
-    # owner = "kradalby";
+    # owner = "juanfont";
+    owner = "kradalby";
     repo = "headscale";
     # rev = "v${version}";
-    rev = "84a5edf3453867e1ff8dd85dcc6771685ff8634b";
-    sha256 = "sha256-MZe8wciK7opsK5355McuQscGzxVVpPOzAfr8b6Sfiwk=";
+    rev = "33ae56acfaf96d6e4d3e8d15c91868b47ba5ed6c";
+    sha256 = "sha256-bmalF5NnXjXzyF8WIIN8burmaYYeZwzDIFoNrZAXX8I=";
   };
 
-  vendorSha256 = "sha256-paDdPsi5OfxsmgX7c5NSDSLYDipFqxxcxV3K4Tc77nQ=";
+  vendorSha256 = "sha256-kc8EU+TkwRlsKM2+ljm/88aWe5h2QMgd/ZGPSgdd9QQ=";
 
   ldflags = [ "-s" "-w" "-X github.com/juanfont/headscale/cmd/headscale/cli.Version=v${version}-${src.rev}" ];
 
