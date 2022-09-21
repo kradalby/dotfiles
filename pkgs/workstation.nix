8a0717916da8a9889350d9f1e1892128e0025321
Kristoffer Dalby <kristoffer@tailscale.com>
2022-09-21 16:49:32 +0200

    Merge two packages files
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- pkgs/workstation.nix
+++ pkgs/workstation.nix
@@ -18,6 +18,7 @@
     step-cli
     colmena
     ragenix
+    ansible
 
     # nix tooling
     nodePackages.node2nix
@@ -31,5 +32,76 @@
 
     python39Packages.pipx
     # osxphotos
+    # Development
+    # buf
+    dockfmt
+    dyff
+    entr
+    gh
+    git-open
+    gitlint
+    # go-jsonnet
+    hadolint
+    html-tidy
+    nodePackages.eslint_d
+
+    # nodePackages.lua-fmt
+
+    nodePackages.prettier
+    # nodejs
+    # poetry
+    pre-commit
+    # rnix-lsp
+    shfmt
+    shellharden
+    shellcheck
+    # yarn
+    editorconfig-checker
+    grpcurl
+    # act
+    vale
+
+    # neovim plugin deps
+    libiconv # rnix-lsp
+    nodejs # ansiblels
+    unstable.dotnet-sdk # omnisharp
+    lua53Packages.luasql-sqlite3 # yank sql
+    lua53Packages.luadbi-sqlite3 # yank sql
+
+    (fenix.complete.withComponents
+      [
+        "cargo"
+        "clippy"
+        "rust-src"
+        "rustc"
+        "rustfmt"
+      ])
+
+    # gofumpt
+    # golangci-lint
+    # gox
+    # golines
+
+    # (lib.mkIf pkgs.stdenv.isLinux swift)
+    (lib.mkIf pkgs.stdenv.isLinux docker)
+
+    # null-ls only support 0.4.0, so override
+    # to ensure we use stable for now.
+    stable.statix
+
+    alejandra
+    deadnix
+
+    nixpkgs-fmt
+    manix
+
+    # Tailscale stuff
+    redo-apenwarr
+
+    clang
+
+    # swiftlint
+    # clang-format
+    # hclfmt
   ];
 }
