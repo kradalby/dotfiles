b8da0dd6120573b2ec1ab642544ee78d5df8c853
Kristoffer Dalby <kradalby@kradalby.no>
2022-10-04 14:38:17 +0200

    Add kraairm2
    Signed-off-by: Kristoffer Dalby <kradalby@kradalby.no>

--- metadata/ssh.nix
+++ metadata/ssh.nix
@@ -5,6 +5,9 @@
 
     # dev.ntnu vm
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGyrauaLwnrgeR5mpeOBCw/creVh1dMU1a12TTXvQ+Rd"
+
+    # kraairm2
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGqesfGzltPA+pNVQ667T1tKzQoz09qTcoQshygxl73I3EbYD5vnHFtC+tnziVbfxSx8ZDRvPDN7vHEalE5U3JU="
   ];
 
   kradalby = [
