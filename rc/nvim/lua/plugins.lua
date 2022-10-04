035d881e975d4502e304514135fcaac124f9d6ab
Kristoffer Dalby <kristoffer@tailscale.com>
2022-10-04 16:44:33 +0200

    use all formaters
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- rc/nvim/lua/plugins.lua
+++ rc/nvim/lua/plugins.lua
@@ -246,7 +246,7 @@ return require("packer").startup(
                                     buffer = bufnr,
                                     callback = function()
                                         -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                                        vim.lsp.buf.formatting_sync()
+                                        vim.lsp.buf.formatting_seq_sync()
                                     end,
                                 })
                             end
