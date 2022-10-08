bbd02acd6533d739eaf9b12241728022dfc47790
Kristoffer Dalby <kradalby@kradalby.no>
2022-10-08 12:32:19 +0200

    Polar party monitoring
    Signed-off-by: Kristoffer Dalby <kradalby@kradalby.no>

---
+++ machines/core.terra/polar/default.nix
@@ -0,0 +1,6 @@
+{
+  imports = [
+    ./grafana.nix
+    ./monitoring.nix
+  ];
+}
---
+++ machines/core.terra/polar/monitoring.nix
@@ -0,0 +1,86 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  prometheusDomain = "prometheus.${config.networking.domain}";
+  # pushgatewayDomain = "pushgateway.${config.networking.domain}";
+in {
+  services.prometheus = {
+    enable = true;
+
+    retentionTime = "14d";
+
+    scrapeConfigs = [
+      {
+        job_name = "consul";
+        consul_sd_configs = [
+          {server = "consul.ldn.fap.no";}
+          {server = "consul.ntnu.fap.no";}
+          {server = "consul.tjoda.fap.no";}
+        ];
+        relabel_configs = [
+          {
+            source_labels = [
+              "__meta_consul_tags"
+            ];
+            regex = ".*,prometheus,.*";
+            action = "keep";
+          }
+          {
+            source_labels = [
+              "__meta_consul_node"
+              "__meta_consul_dc"
+              "__meta_consul_service_port"
+            ];
+            regex = "([a-z]+);([a-z]+);([0-9]+)";
+            replacement = "$1.$2.fap.no:$3";
+            target_label = "instance";
+          }
+          {
+            source_labels = [
+              "__meta_consul_dc"
+            ];
+            replacement = "$1";
+            target_label = "site";
+          }
+          {
+            source_labels = [
+              "__meta_consul_service"
+            ];
+            target_label = "job";
+          }
+        ];
+      }
+    ];
+  };
+
+  security.acme.certs."${prometheusDomain}".domain = prometheusDomain;
+
+  services.nginx.virtualHosts."${prometheusDomain}" = {
+    forceSSL = true;
+    useACMEHost = prometheusDomain;
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:${toString config.services.prometheus.port}";
+      # proxyWebsockets = true;
+    };
+    extraConfig = ''
+      access_log /var/log/nginx/${prometheusDomain}.access.log;
+    '';
+  };
+
+  # security.acme.certs."${pushgatewayDomain}".domain = pushgatewayDomain;
+  #
+  # services.nginx.virtualHosts."${pushgatewayDomain}" = {
+  #   forceSSL = true;
+  #   useACMEHost = pushgatewayDomain;
+  #   locations."/" = {
+  #     proxyPass = "http://${config.services.prometheus.pushgateway.web.listen-address}";
+  #     # proxyWebsockets = true;
+  #   };
+  #   extraConfig = ''
+  #     access_log /var/log/nginx/${pushgatewayDomain}.access.log;
+  #   '';
+  # };
+}
