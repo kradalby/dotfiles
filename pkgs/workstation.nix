64f17c29528490b04c44c0314c1883d4c26aec27
Kristoffer Dalby <kristoffer@tailscale.com>
2022-12-20 11:10:56 +0100

    add fix script for touchid pam sudo
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- pkgs/workstation.nix
+++ pkgs/workstation.nix
@@ -97,6 +97,21 @@
       cook-cli
 
       silicon
+
+      (pkgs.writeScriptBin
+        "pamtouchfix"
+        ''
+          #!/run/current-system/sw/bin/bash
+          cat <<EOT > /etc/pam.d/sudo
+          auth       optional       /opt/homebrew/lib/pam/pam_reattach.so
+          auth       sufficient     pam_tid.so
+          auth       sufficient     pam_smartcard.so
+          auth       required       pam_opendirectory.so
+          account    required       pam_permit.so
+          password   required       pam_deny.so
+          session    required       pam_permit.so
+          EOT
+        '')
     ]
     ++ lib.optionals stdenv.isLinux [
       # swift
