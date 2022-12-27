aef391c3b410dfb3b05563b9fb711b6a9884dc67
Kristoffer Dalby <kristoffer@dalby.cc>
2022-12-27 21:29:19 +0100

    Playing with nixos on nanopi2
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

---
+++ misc/nanopi-neo2/default.nix
@@ -0,0 +1,32 @@
+{...}: {
+  imports = [
+    ../../common/journal-volatile.nix
+  ];
+
+  nixpkgs.overlays = [
+    (import ./overlay.nix)
+  ];
+
+  hardware.enableRedistributableFirmware = true;
+  services.getty.autologinUser = "root";
+  sdImage.compressImage = false;
+
+  boot = {
+    kernelPackages = pkgs.linuxPackagesNanopiNeo2;
+    consoleLogLevel = lib.mkDefault 7;
+    tmpOnTmpfs = true;
+    kernelParams = [
+      "cma=32M"
+      "console=ttyS2,115200n8"
+      "console=ttyACM0,115200n8"
+      "console=tty0"
+    ];
+
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+  };
+
+  hardware.enableRedistributableFirmware = true;
+}
