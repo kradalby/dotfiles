185348f5823987825f4f1a789d13c68bd299daa0
Kristoffer Dalby <kristoffer@tailscale.com>
2023-01-03 11:23:52 +0100

    system platform specific list
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- pkgs/system.nix
+++ pkgs/system.nix
@@ -3,64 +3,65 @@
   lib,
   ...
 }: {
-  environment.systemPackages = with pkgs; [
-    fish
+  environment.systemPackages = with pkgs;
+    [
+      fish
 
-    neovim
+      neovim
 
-    # jemalloc, which bind depends on is broken on darwin aarch64
-    (lib.mkIf (! (pkgs.stdenv.isDarwin && pkgs.stdenv.isAarch64)) dig)
-    vim
-    babelfish
-    bat
-    coreutils
-    dos2unix
-    exa
-    fd
-    fdupes
-    fish
-    fzf
-    git
-    htop
-    jq
-    lsof
-    mosh
-    ncdu
-    p7zip
-    procs
-    rclone
-    restic
-    ripgrep
-    socat
-    tldr
-    tmux
-    tree
-    unzip
-    wget
-    yq
-    rsync
-    viddy
-    zstd
-    nftables
+      # jemalloc, which bind depends on is broken on darwin aarch64
+      (lib.mkIf (! (pkgs.stdenv.isDarwin && pkgs.stdenv.isAarch64)) dig)
+      vim
+      babelfish
+      bat
+      coreutils
+      dos2unix
+      exa
+      fd
+      fdupes
+      fish
+      fzf
+      git
+      htop
+      jq
+      lsof
+      mosh
+      ncdu
+      p7zip
+      procs
+      rclone
+      restic
+      ripgrep
+      socat
+      tldr
+      tmux
+      tree
+      unzip
+      wget
+      yq
+      rsync
+      viddy
+      zstd
 
-    # Linux only
-    (lib.mkIf pkgs.stdenv.isLinux usbutils)
+      # Neovim plugins
+      gcc
 
-    # Neovim plugins
-    gcc
+      # Nix tooling
+      nixpkgs-fmt
+      nix-diff
+      nix-tree
 
-    # Nix tooling
-    nixpkgs-fmt
-    nix-diff
-    nix-tree
-
-    (let
-      my-python-packages = python-packages:
-        with python-packages; [
-          requests
-        ];
-      python-with-my-packages = python3.withPackages my-python-packages;
-    in
-      python-with-my-packages)
-  ];
+      (let
+        my-python-packages = python-packages:
+          with python-packages; [
+            requests
+          ];
+        python-with-my-packages = python3.withPackages my-python-packages;
+      in
+        python-with-my-packages)
+    ]
+    ++ lib.optionals stdenv.isLinux [
+      nftables
+      usbutils
+    ];
 }
