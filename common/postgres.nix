73762206076c6a353cd615cfaf749f21b466b507
Kristoffer Dalby <kristoffer@tailscale.com>
2023-02-18 07:52:16 -0700

    posgres: generalise
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

---
+++ common/postgres.nix
@@ -0,0 +1,47 @@
+{ pkgs
+, lib
+, config
+, ...
+}: {
+  options = {
+    my.postgres = {
+      databases = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [ ];
+      };
+
+      extraBackups = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [ ];
+      };
+    };
+  };
+
+  config = {
+    services.postgresql = {
+      enable = true;
+
+      package = pkgs.postgresql_14;
+
+      ensureUsers =
+        builtins.map
+          (
+            database: {
+              name = database;
+              ensurePermissions = {
+                "DATABASE ${database}" = "ALL PRIVILEGES";
+              };
+            }
+          )
+          config.my.postgres.databases;
+
+      ensureDatabases = config.my.postgres.databases;
+    };
+
+    services.postgresqlBackup = {
+      enable = true;
+
+      databases = config.my.postgres.databases ++ config.my.postgres.extraBackups;
+    };
+  };
+}
