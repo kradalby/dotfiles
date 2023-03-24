410dc60dc2b82b68b1195616a16dc4ef87044bc6
Kristoffer Dalby <kristoffer@tailscale.com>
2023-03-24 17:02:08 +0100

    machine: add dev.ldn
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- machines/dev.ldn/default.nix
+++ machines/dev.ldn/default.nix
@@ -14,40 +14,22 @@
 
     ../../common/coredns.nix
     ../../common/consul-server.nix
-    ../../common/miniupnp.nix
 
-    ./restic.nix
-    ./wireguard.nix
+    # ./restic.nix
     ./tailscale.nix
-    ./kuma.nix
-    ./monitoring.nix
-    ./loki.nix
-    ./grafana.nix
-    ./step-ca.nix
-    ./openvpn.nix
-    ./postgres.nix
-    ./keycloak.nix
-    # ./glauth.nix
-    ./nextcloud.nix
-    ./minio.nix
-    ./rustdesk-server.nix
-
-    # ../../modules/umami.nix
-    # ./umami.nix
-    ./golink.nix
-    ./webpage.nix
+    # ./syncthing.nix
   ];
 
-  my.wan = "enp0s3";
-  my.lan = "enp1s0";
+  my.wan = "enp2s0";
+  my.lan = "enp3s0";
 
-  environment.systemPackages = with pkgs; [
-    rustdesk-server
-  ];
+  age.secrets.ldn-wifi = {
+    file = ../../secrets/ldn-wifi.age;
+  };
 
   networking = {
-    hostName = "core";
-    domain = "oracldn.fap.no";
+    hostName = "dev";
+    domain = "ldn.fap.no";
     nameservers = [
       "1.1.1.1"
       "1.0.0.1"
@@ -57,41 +39,38 @@
       "${config.my.wan}" = {
         useDHCP = true;
       };
+    };
 
-      ${config.my.lan} = {
-        useDHCP = false;
-        ipv4.addresses = [
-          {
-            address = "10.66.0.1";
-            prefixLength = 24;
-          }
-        ];
-        tempAddress = "disabled";
+    wireless = {
+      enable = true;
+      environmentFile = config.age.secrets.ldn-wifi.path;
+      networks = {
+        "_kad".psk = "@PSK_UNDERSCORE_KAD@";
       };
     };
 
     nat = {
       enable = true;
       externalInterface = config.my.wan;
-      internalIPs = [ "10.0.0.0/8" ];
-      internalInterfaces = [ config.my.lan "iot" ];
-      forwardPorts = [
-        {
-          sourcePort = 64322;
-          destination = "10.66.0.1:22";
-          proto = "tcp";
-        }
-        {
-          sourcePort = 500;
-          destination = "10.66.0.1:51820";
-          proto = "udp";
-        }
-        {
-          sourcePort = 4500;
-          destination = "10.66.0.1:51820";
-          proto = "udp";
-        }
-      ];
+      # internalIPs = [ "10.0.0.0/8" ];
+      # internalInterfaces = [ config.my.lan "iot" ];
+      # forwardPorts = [
+      #   {
+      #     sourcePort = 64322;
+      #     destination = "10.67.0.1:22";
+      #     proto = "tcp";
+      #   }
+      #   {
+      #     sourcePort = 500;
+      #     destination = "10.67.0.1:51820";
+      #     proto = "udp";
+      #   }
+      #   {
+      #     sourcePort = 4500;
+      #     destination = "10.67.0.1:51820";
+      #     proto = "udp";
+      #   }
+      # ];
     };
 
     firewall = {
@@ -103,25 +82,19 @@
         22 # SSH
         80 # HTTP
         443 # HTTPS
-
-        21115
-        21116
-        21117
-        21118
-        21119 # Rustdesk
       ];
 
       allowedUDPPorts = lib.mkForce [
         443 # HTTPS
         config.services.tailscale.port
-        config.networking.wireguard.interfaces.wg0.listenPort
-
-        21116 # Rustdesk
+        # config.networking.wireguard.interfaces.wg0.listenPort
       ];
 
       trustedInterfaces = [ config.my.lan ];
     };
   };
+
+  virtualisation.docker.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
---
+++ machines/dev.ldn/syncthing.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  flakes,
+  pkgs,
+  lib,
+  ...
+}: let
+  nginx = import ../../common/funcs/nginx.nix {inherit config lib;};
+
+  cfg = import ../../metadata/syncthing.nix;
+  domain = "syncthing.dev.${config.networking.domain}";
+in
+  lib.mkMerge [
+    {
+      services = {
+        syncthing = {
+          inherit (cfg) devices;
+          user = "kradalby";
+          dataDir = "/home/kradalby";
+          enable = true;
+          overrideDevices = true;
+          overrideFolders = true;
+          folders = {
+            "kradalby - Sync" = {
+              id = "xTDuT-kZeuK";
+              # Name of folder in Syncthing, also the folder ID
+              path = "/home/kradalby/Sync"; # Which folder to add to Syncthing
+              devices = builtins.attrNames config.services.syncthing.devices; # Which devices to share the folder with
+              type = "sendreceive";
+            };
+          };
+        };
+      };
+    }
+
+    (nginx.internalVhost {
+      inherit domain;
+      proxyPass = "http://127.0.0.1:8384";
+    })
+  ]
