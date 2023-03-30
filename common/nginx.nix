6478df0e9704f92b6a9b935e762b0fd255a88858
Kristoffer Dalby <kristoffer@tailscale.com>
2023-03-30 14:18:33 +0200

    nginx: disable broken exporter
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- common/nginx.nix
+++ common/nginx.nix
@@ -1,11 +1,12 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+let
+  consul = import ./funcs/consul.nix { inherit lib; };
+in
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}: let
-  consul = import ./funcs/consul.nix {inherit lib;};
-in {
   options.my.enableSslh = lib.mkOption {
     type = lib.types.bool;
     default = false;
@@ -94,14 +95,14 @@ in {
       '';
     };
 
-    networking.firewall.allowedTCPPorts = [80];
+    networking.firewall.allowedTCPPorts = [ 80 ];
 
-    services.prometheus.exporters.nginx = {
-      enable = true;
-      openFirewall = true;
-    };
-
-    my.consulServices.nginx_exporter = consul.prometheusExporter "nginx" config.services.prometheus.exporters.nginx.port;
+    # services.prometheus.exporters.nginx = {
+    #   enable = true;
+    #   openFirewall = true;
+    # };
+    #
+    # my.consulServices.nginx_exporter = consul.prometheusExporter "nginx" config.services.prometheus.exporters.nginx.port;
 
     services.prometheus.exporters.nginxlog = {
       enable = true;
@@ -111,22 +112,23 @@ in {
       user = "nginx";
 
       settings = {
-        namespaces = let
-          # format = ''
-          #   $remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent"
-          # '';
-          mkApp = domain: {
-            name = domain;
-            metrics_override = {prefix = "nginxlog";};
-            source.files = ["/var/log/nginx/${domain}.access.log"];
-            namespace_label = "vhost";
-          };
-        in
+        namespaces =
+          let
+            # format = ''
+            #   $remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent"
+            # '';
+            mkApp = domain: {
+              name = domain;
+              metrics_override = { prefix = "nginxlog"; };
+              source.files = [ "/var/log/nginx/${domain}.access.log" ];
+              namespace_label = "vhost";
+            };
+          in
           [
             {
               name = "catch";
-              metrics_override = {prefix = "nginxlog";};
-              source.files = ["/var/log/nginx/access.log"];
+              metrics_override = { prefix = "nginxlog"; };
+              source.files = [ "/var/log/nginx/access.log" ];
               namespace_label = "vhost";
             }
           ]
