23a93d7de42430a36e2587cb5035c6981ca3e6ee
Kristoffer Dalby <kristoffer@dalby.cc>
2023-04-02 16:12:18 +0200

    unifi: use unstable module
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/core.tjoda/unifi.nix
+++ machines/core.tjoda/unifi.nix
@@ -1,6 +1,7 @@
 { pkgs
 , config
 , lib
+, flakes
 , ...
 }:
 let
@@ -9,61 +10,68 @@ let
 
   domain = "unifi.${config.networking.domain}";
 in
-lib.mkMerge [
-  {
-    services.unifi = {
-      unifiPackage = pkgs.unifi.overrideAttrs (attrs: {
-        meta = attrs.meta // { license = lib.licenses.mit; };
-      });
-      enable = true;
-      openFirewall = true;
+# lib.mkMerge [
+{
+  disabledModules = [ "services/networking/unifi.nix" ];
 
-      # initialJavaHeapSize = 1024;
-      # maximumJavaHeapSize = 1536;
-    };
+  imports = [
+    "${flakes.nixpkgs-unstable}/nixos/modules/services/networking/unifi.nix"
+  ];
 
-    # TODO: Remove 8443 when nginx can correctly proxy
-    networking.firewall.allowedTCPPorts = [ 8443 9130 ];
+  services.unifi = {
+    unifiPackage = pkgs.unstable.unifi.overrideAttrs (attrs: {
+      meta = attrs.meta // { license = lib.licenses.mit; };
+    });
+    # jrePackage = pkgs.unstable.jre8;
+    mongodbPackage = pkgs.mongodb;
+    enable = true;
+    openFirewall = true;
 
-    # age.secrets.unifi-tjoda-read-only = {
-    #   file = ../../secrets/unifi-tjoda-read-only.age;
-    #   mode = "0400";
-    #   owner = "unifi-poller";
-    # };
+    # initialJavaHeapSize = 1024;
+    # maximumJavaHeapSize = 1536;
+  };
 
-    # services.unifi-poller = {
-    #   enable = true;
-    #
-    #   unifi.defaults = {
-    #     url = "https://127.0.0.1:8443";
-    #     user = "read-only";
-    #     pass = config.age.secrets.unifi-tjoda-read-only.path;
-    #
-    #     verify_ssl = false;
-    #   };
-    #
-    #   influxdb.disable = true;
-    #
-    #   prometheus = {
-    #     http_listen = ":9130";
-    #   };
-    # };
+  # TODO: Remove 8443 when nginx can correctly proxy
+  networking.firewall.allowedTCPPorts = [ 8443 9130 ];
 
-    # my.consulServices.unifi_exporter = consul.prometheusExporter "unifi" config.services.prometheus.exporters.unifi.port;
-  }
+  # age.secrets.unifi-tjoda-read-only = {
+  #   file = ../../secrets/unifi-tjoda-read-only.age;
+  #   mode = "0400";
+  #   owner = "unifi-poller";
+  # };
 
-  (nginx.internalVhost {
-    inherit domain;
+  # services.unifi-poller = {
+  #   enable = true;
+  #
+  #   unifi.defaults = {
+  #     url = "https://127.0.0.1:8443";
+  #     user = "read-only";
+  #     pass = config.age.secrets.unifi-tjoda-read-only.path;
+  #
+  #     verify_ssl = false;
+  #   };
+  #
+  #   influxdb.disable = true;
+  #
+  #   prometheus = {
+  #     http_listen = ":9130";
+  #   };
+  # };
 
-    proxyPass = "https://localhost:8443/";
-    proxyWebsockets = true;
-    locationExtraConfig = ''
-      proxy_set_header Accept-Encoding "";
-      proxy_set_header X-Real-IP $remote_addr;
-      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-      proxy_set_header X-Forwarded-Proto $scheme;
-      proxy_set_header Front-End-Https on;
-      proxy_redirect off;
-    '';
-  })
-]
+  # my.consulServices.unifi_exporter = consul.prometheusExporter "unifi" config.services.prometheus.exporters.unifi.port;
+}
+#   (nginx.internalVhost {
+#     inherit domain;
+#
+#     proxyPass = "https://localhost:8443/";
+#     proxyWebsockets = true;
+#     locationExtraConfig = ''
+#       proxy_set_header Accept-Encoding "";
+#       proxy_set_header X-Real-IP $remote_addr;
+#       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+#       proxy_set_header X-Forwarded-Proto $scheme;
+#       proxy_set_header Front-End-Https on;
+#       proxy_redirect off;
+#     '';
+#   })
+# ]
