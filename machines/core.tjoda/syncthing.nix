9d07529347f0aa92662a78637f92b158a593af33
Kristoffer Dalby <kristoffer@dalby.cc>
2023-04-02 16:12:55 +0200

    syncthing: use sendreceive
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/core.tjoda/syncthing.nix
+++ machines/core.tjoda/syncthing.nix
@@ -1,68 +1,68 @@
-{
-  config,
-  flakes,
-  pkgs,
-  lib,
-  ...
-}: let
-  nginx = import ../../common/funcs/nginx.nix {inherit config lib;};
+{ config
+, flakes
+, pkgs
+, lib
+, ...
+}:
+let
+  nginx = import ../../common/funcs/nginx.nix { inherit config lib; };
 
   cfg = import ../../metadata/syncthing.nix;
   domain = "syncthing.core.${config.networking.domain}";
 in
-  lib.mkMerge [
-    {
-      services = {
-        syncthing = {
-          inherit (cfg) devices;
-          user = "storage";
-          group = "storage";
-          dataDir = "/storage";
-          enable = true;
-          overrideDevices = true;
-          overrideFolders = true;
-          folders = {
-            "/storage/software" = {
-              id = "vpgyn-cj2mg";
-              path = "/storage/software";
-              devices = cfg.storage;
-              type = "receiveonly";
-            };
+lib.mkMerge [
+  {
+    services = {
+      syncthing = {
+        inherit (cfg) devices;
+        user = "storage";
+        group = "storage";
+        dataDir = "/storage";
+        enable = true;
+        overrideDevices = true;
+        overrideFolders = true;
+        folders = {
+          "/storage/software" = {
+            id = "vpgyn-cj2mg";
+            path = "/storage/software";
+            devices = cfg.storage;
+            type = "sendreceive";
+          };
 
-            "/storage/pictures" = {
-              id = "orqnv-bg72d";
-              path = "/storage/pictures";
-              devices = cfg.storage;
-              type = "receiveonly";
-            };
+          "/storage/pictures" = {
+            id = "orqnv-bg72d";
+            path = "/storage/pictures";
+            devices = cfg.storage;
+            type = "sendreceive";
+          };
 
-            "/storage/backup" = {
-              id = "9bjac-k65uu";
-              path = "/storage/backup";
-              devices = cfg.storage;
-              type = "receiveonly";
-            };
+          "/storage/backup" = {
+            id = "9bjac-k65uu";
+            path = "/storage/backup";
+            devices = cfg.storage;
+            type = "sendreceive";
+          };
 
-            "/storage/books" = {
-              id = "ww4gn-xgy9i";
-              path = "/storage/books";
-              devices = cfg.storage;
-              type = "receiveonly";
-            };
+          "/storage/books" = {
+            id = "ww4gn-xgy9i";
+            path = "/storage/books";
+            devices = cfg.storage;
+            type = "sendreceive";
+          };
 
-            "kradalby - Sync" = {
-              id = "xTDuT-kZeuK";
-              path = "/storage/sync/kradalby";
-              devices = builtins.attrNames config.services.syncthing.devices;
-              type = "receiveonly";
-            };
+          "kradalby - Sync" = {
+            id = "xTDuT-kZeuK";
+            path = "/storage/sync/kradalby";
+            devices = builtins.attrNames config.services.syncthing.devices;
+            type = "sendreceive";
           };
         };
       };
-    }
+    };
+  }
 
-    (nginx.internalVhost {
-      inherit domain;
-      proxyPass = "http://127.0.0.1:8384";
-    })
-  ]
+  (nginx.internalVhost {
+    inherit domain;
+    proxyPass = "http://127.0.0.1:8384";
+  })
+]
