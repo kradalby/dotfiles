9bbdf988a8dc7f4e446089456fe40f6c51fe014d
Kristoffer Dalby <kristoffer@dalby.cc>
2023-07-07 16:02:10 +0200

    hugin: public and basic auth
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/core.terra/hugin.nix
+++ machines/core.terra/hugin.nix
@@ -1,24 +1,40 @@
 {
-  pkgs,
+  lib,
   config,
   ...
-}: {
-  age.secrets.hugin-tskey = {
-    file = ../../secrets/hugin-tskey.age;
-    owner = "storage";
+}: let
+  domain = "hugin.kradalby.no";
+  nginx = import ../../common/funcs/nginx.nix {inherit config lib;};
+
+  vhost = nginx.externalVhost {
+    inherit domain;
+    proxyPass = "http://127.0.0.1:${toString config.services.hugin.localhostPort}";
+    basicAuthFile = config.age.secrets.hugin-basicauth.path;
   };
+in
+  lib.mkMerge [
+    {
+      age.secrets.hugin-tskey = {
+        file = ../../secrets/hugin-tskey.age;
+        owner = "storage";
+      };
 
-  services.hugin = {
-    enable = true;
-    tailscaleKeyPath = config.age.secrets.hugin-tskey.path;
+      age.secrets.hugin-basicauth = {
+        file = ../../secrets/hugin-basicauth.age;
+        owner = "nginx";
+      };
 
-    verbose = true;
+      services.hugin = {
+        enable = true;
+        tailscaleKeyPath = config.age.secrets.hugin-tskey.path;
 
-    # package = pkgs.hugin;
+        verbose = true;
 
-    user = "storage";
-    group = "storage";
+        user = "storage";
+        group = "storage";
 
-    album = "/fast/hugin/album";
-  };
-}
+        album = "/fast/hugin/album";
+      };
+    }
+    vhost
+  ]
