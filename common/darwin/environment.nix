b90532f27e498c9e6196fb29dc9e0d9ca77f7dc7
Kristoffer Dalby <kristoffer@tailscale.com>
2023-07-24 08:53:27 +0200

    darwin: remove pam override for sudo
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- common/darwin/environment.nix
+++ common/darwin/environment.nix
@@ -4,15 +4,15 @@
   stdenv,
   ...
 }: {
-  environment.etc = lib.mkIf pkgs.stdenv.isAarch64 {
-    "pam.d/sudo".text = ''
-      auth       optional       /opt/homebrew/lib/pam/pam_reattach.so
-      auth       sufficient     pam_tid.so
-      auth       sufficient     pam_smartcard.so
-      auth       required       pam_opendirectory.so
-      account    required       pam_permit.so
-      password   required       pam_deny.so
-      session    required       pam_permit.so
-    '';
-  };
+  # environment.etc = lib.mkIf pkgs.stdenv.isAarch64 {
+  #   "pam.d/sudo".text = ''
+  #     auth       optional       /opt/homebrew/lib/pam/pam_reattach.so
+  #     auth       sufficient     pam_tid.so
+  #     auth       sufficient     pam_smartcard.so
+  #     auth       required       pam_opendirectory.so
+  #     account    required       pam_permit.so
+  #     password   required       pam_deny.so
+  #     session    required       pam_permit.so
+  #   '';
+  # };
 }
