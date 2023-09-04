0128e5a7567f09a0b5ff95b3908ceb1f97db1eab
Kristoffer Dalby <kristoffer@dalby.cc>
2023-09-04 13:59:10 +0200

    services: add ghostfolio and stirling
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/core.oracldn/ghostfolio.nix
+++ machines/core.oracldn/ghostfolio.nix
@@ -15,6 +15,11 @@
 in
   lib.mkMerge [
     {
+      age.secrets.ghostfolio-env = {
+        file = ../../secrets/ghostfolio-env.age;
+        owner = "ghostfolio";
+      };
+
       users.users.ghostfolio = {
         home = "/var/lib/ghostfolio";
         createHome = true;
@@ -26,12 +31,15 @@ in
 
       users.groups.ghostfolio = {};
 
-      # services.redis.servers.ghostfolio = {
-      #   enable = true;
-      #
-      #   user = config.users.users.ghostfolio.uid;
-      #   requirePass = "not-that-important";
-      # };
+      services.redis.servers.ghostfolio = {
+        enable = true;
+
+        bind = "172.17.0.1";
+        port = 6379;
+
+        user = "ghostfolio";
+        requirePass = "not-that-important";
+      };
 
       virtualisation.oci-containers.containers.ghostfolio = {
         image = "ghostfolio/ghostfolio:1.305.0";
@@ -39,16 +47,18 @@ in
         # workdir = "/home/podmanager";
         autoStart = true;
         ports = [
-          "3333:${toString port}/tcp"
+          "${toString port}:3333/tcp"
         ];
         environment = {
-          ACCESS_TOKEN_SALT = "bg#cnuVNbP9TqSKpPT&KD#w8m@$Y7&Yc";
-          JWT_SECRET_KEY = "F5&VA8bsLSfcQHxB!izq3qfUvm^JysQJ";
           DATABASE_URL = "postgresql://ghostfolio@172.17.0.1/ghostfolio";
           NODE_ENV = "production";
-          # REDIS_HOST = "redis";
-          # REDIS_PASSWORD = config.services.redis.servers.ghostfolio.requirePass;
+          REDIS_HOST = "172.17.0.1";
+          REDIS_PORT = toString config.services.redis.servers.ghostfolio.port;
+          REDIS_PASSWORD = config.services.redis.servers.ghostfolio.requirePass;
         };
+        environmentFiles = [
+          config.age.secrets.ghostfolio-env.path
+        ];
         volumes = [
           "/run/postgresql:/run/postgresql"
           # "${config.services.redis.servers.ghostfolio.unixSocket}:/run/redis"
