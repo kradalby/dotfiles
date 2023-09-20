f0b681586300e05c5ea11fd3da0b95114d42276f
Kristoffer Dalby <kristoffer@tailscale.com>
2023-09-20 09:15:34 -0500

    neovim: move from efm to null-ls
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- pkgs/neovim.nix
+++ pkgs/neovim.nix
@@ -1,78 +1,74 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    # Tools
+    ## Tools
     fd
     ripgrep
     lua53Packages.luadbi-sqlite3 # yank sql
     lua53Packages.luasql-sqlite3 # yank sql
     libiconv # rnix-lsp
 
-    # LSP
-    efm-langserver
-    unstable.gopls
-    nil
-    nodePackages.yaml-language-server
-    nodePackages.vscode-langservers-extracted
-    # rnix-lsp
-    unstable.nixd
-    sumneko-lua-language-server
-    terraform-ls
+    ## LSP
+    ansible-language-server
     buf-language-server
     dhall-lsp-server
-    golangci-lint-langserver
-    rust-analyzer
+    efm-langserver
     elmPackages.elm-language-server
-    nodePackages.typescript-language-server
+    golangci-lint-langserver
+    nil
     nodePackages.typescript
+    nodePackages.typescript-language-server
+    nodePackages.vscode-langservers-extracted
+    nodePackages.yaml-language-server
     nodePackages_latest.pyright
-    ansible-language-server
+    rust-analyzer
+    sumneko-lua-language-server
+    terraform-ls
+    unstable.gopls
+    unstable.nixd
 
-    # EFM
-    buf
-    python310Packages.flake8
-    mypy
-    nodePackages.stylelint
-    nodePackages.prettier
-    shfmt
-    yamllint
-    golines
-    gofumpt
-
-    # null-ls
+    ## null-ls
+    # curlylint
+    # djlint
     actionlint
     alejandra
     beautysh
     black
+    buf
     cbfmt
     clang
     commitlint
-    # curlylint
     deadnix
-    # djlint
     editorconfig-checker
     gitlint
+    go-tools # staticcheck
+    gofumpt
+    golines
+    gotools # goimports
     html-tidy
     isort
     jq
-    nodePackages.prettier_d_slim
+    mypy
     nodePackages.eslint_d
+    nodePackages.prettier_d_slim
+    nodePackages.stylelint
+    nodePackages.write-good
     proselint
+    python310Packages.flake8
     python310Packages.pylama
     shellcheck
     shellharden
-    go-tools # staticcheck
-    gotools # goimports
+    shfmt
     statix
     vale
-    nodePackages.write-good
+    yamllint
 
-    # Debug
+    ## Debug
 
     # Use unstable to ensure we get a modern version,
     # we need to have one that is at least as new as Go.
     unstable.delve
 
-    # Unconfigured
+    ## Unconfigured
     dockfmt
     gh
     hadolint
