ebecc22a9dddc459f8c7e5884c152793c2579c94
Kristoffer Dalby <kristoffer@dalby.cc>
2023-10-22 11:47:14 +0200

    home.ldn: add isponsortv, remove unifi
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/home.ldn/iSponsorBlockTV.nix
+++ machines/home.ldn/iSponsorBlockTV.nix
@@ -1,43 +1,17 @@
+{...}:
+# Config:
+#  docker run --rm -v '/var/lib/isponsorblocktv:/app/data' -it ghcr.io/dmunozv04/isponsorblocktv:latest --setup-cli
 {
-  config,
-  pkgs,
-  lib,
-  ...
-}: let
-  # domain = "pdf.fap.no";
-  # port = 63456;
-  # nginx = import ../../common/funcs/nginx.nix {inherit config lib;};
-  #
-  # vhost = nginx.internalVhost {
-  #   inherit domain;
-  #   proxyPass = "http://127.0.0.1:${toString port}";
-  # };
-in
-  # lib.mkMerge [
-  {
-    # users.users.stirling = {
-    #   home = "/var/lib/stirling";
-    #   createHome = true;
-    #   group = "stirling";
-    #   isSystemUser = true;
-    #   isNormalUser = false;
-    #   description = "Stirling PDF";
-    # };
-    #
-    # users.groups.stirling = {};
-
-    virtualisation.oci-containers.containers.isponsorblocktv = {
-      image = "ghcr.io/dmunozv04/isponsorblocktv:latest";
-      # user = config.users.users.stirling.uid;
-      autoStart = true;
-      # ports = [
-      #   "${toString port}:8080/tcp"
-      # ];
-      environment = {};
-      volumes = [
-        "/var/lib/isponsorblocktv:/app/data"
-      ];
-    };
-  }
-#   vhost
-# ]
+  virtualisation.oci-containers.containers.isponsorblocktv = {
+    image = "ghcr.io/dmunozv04/isponsorblocktv:latest";
+    # user = config.users.users.stirling.uid;
+    autoStart = true;
+    # ports = [
+    #   "${toString port}:8080/tcp"
+    # ];
+    environment = {};
+    volumes = [
+      "/var/lib/isponsorblocktv:/app/data"
+    ];
+  };
+}
