a6f9fa5db6a8ec8b11dcb8e80965150616808093
Kristoffer Dalby <kristoffer@dalby.cc>
2023-10-23 22:06:08 +0200

    home.ldn: fix scrypted
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/home.ldn/scrypted.nix
+++ machines/home.ldn/scrypted.nix
@@ -1,78 +1,46 @@
 {
-  config,
   pkgs,
+  config,
   lib,
-  system,
+  flakes,
   ...
 }: let
+  nginx = import ../../common/funcs/nginx.nix {inherit config lib;};
+
   domain = "scrypted.${config.networking.domain}";
+in
+  lib.mkMerge [
+    {
+      virtualisation.oci-containers.containers.scrypted = {
+        image = "koush/scrypted:latest";
+        # user = config.users.users.stirling.uid;
+        autoStart = true;
+        # ports = [
+        #   "${toString port}:8080/tcp"
+        # ];
 
-  dataDir = "/var/lib/scrypted";
-
-  scryptedPackages = import ../../modules/scrypted/override.nix {inherit pkgs system;};
-  packageModulePath = package: "${package}/lib/node_modules/";
-  nodeModulePaths = map packageModulePath (builtins.attrValues scryptedPackages);
-  nodePath = builtins.concatStringsSep ":" nodeModulePaths;
-  scryptedWrapped = pkgs.stdenv.mkDerivation rec {
-    version = "1.0.0";
-    name = "scrypted-${version}";
-    unpackPhase = "true";
-    buildPhase = "true";
-    installPhase = ''
-      mkdir -p $out/bin
-      cat > $out/bin/scrypted <<EOF
-      #!/bin/sh
-      NODE_PATH=${nodePath} exec ${scryptedPackages.scrypted}/bin/scrypted serve
-      EOF
-      chmod +x $out/bin/scrypted
-    '';
-  };
-in {
-  users.users.scrypted = {
-    home = dataDir;
-    createHome = true;
-    group = "scrypted";
-    isSystemUser = false;
-    isNormalUser = true;
-    description = "Home Bridge";
-  };
-
-  users.groups.scrypted = {};
-
-  # networking.firewall.allowedTCPPorts = [ scryptedConfig.bridge.port ];
-  # networking.firewall.allowedUDPPorts = [ scryptedConfig.bridge.port 1900 5350 5351 5353 ];
-
-  systemd.services.scrypted = {
-    enable = true;
-    restartTriggers = [scryptedWrapped];
-    script = "exec ${scryptedWrapped}/bin/scrypted";
-    wantedBy = ["multi-user.target"];
-    after = ["network.target"];
-    serviceConfig = {
-      User = "scrypted";
-      Restart = "always";
-      RestartSec = "15";
-      # CapabilityBoundingSet = "CAP_NET_RAW";
-      AmbientCapabilities = "CAP_NET_RAW";
-    };
-    environment = {};
-
-    # preStart = ''
-    #   cp -f ${configFile} ${dataDir}/config.json
-    # '';
-  };
-
-  # security.acme.certs."${domain}".domain = domain;
-  #
-  # services.nginx.virtualHosts."${domain}" = {
-  #   forceSSL = true;
-  #   useACMEHost = domain;
-  #   locations."/" = {
-  #     proxyPass = "http://127.0.0.1:${toString scryptedUIPort}";
-  #     # proxyWebsockets = true;
-  #   };
-  # extraConfig = ''
-  #   access_log /var/log/nginx/${domain}.access.log;
-  # '';
-  # };
-}
+        environment = {
+          SCRYPTED_DOCKER_AVAHI = "true";
+        };
+        ports = [
+          "10443:10443/tcp"
+        ];
+        volumes = [
+          "/var/lib/scrypted:/server/volume"
+        ];
+        extraOptions = [
+          "--network=host"
+        ];
+      };
+    }
+    (nginx.internalVhost {
+      inherit domain;
+      proxyPass = "https://127.0.0.1:10443";
+      locationExtraConfig = ''
+        proxy_ssl_verify              off;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection $http_upgrade;
+        proxy_buffering off;
+      '';
+    })
+  ]
