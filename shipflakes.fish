69c3768663bee287990abfd124815e31ba0e93f4
Kristoffer Dalby <kristoffer@dalby.cc>
2023-12-20 09:54:37 +0100

    ship: add hostname
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- shipflakes.fish
+++ shipflakes.fish
@@ -3,5 +3,6 @@
 # This script ships this directory to the machines, useful if Colmena isnt behaving
 
 rg "= nixosBox" --no-heading | awk '{print $2}' | rg -v "#" | tr -d '"' | while read host
+    echo "Shipping to $host..."
     rsync -avh --progress --delete --cvs-exclude --filter=':- .gitignore' . $host:/etc/nixos/.
 end
