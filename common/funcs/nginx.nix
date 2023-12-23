f0ce817c4369037f1d11458c2e2333e390bb01b1
Kristoffer Dalby <kristoffer@dalby.cc>
2023-12-23 23:37:47 +0100

    nginx: pass on ip in heads
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- common/funcs/nginx.nix
+++ common/funcs/nginx.nix
@@ -49,6 +49,11 @@
       locations."/" = {
         inherit proxyPass;
         inherit proxyWebsockets;
+        extraConfig = ''
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header Host $host;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        '';
       };
       inherit basicAuthFile;
       extraConfig = ''
