eb8c7bcc8b94a0807d3495a7984bb2bd9867a114
Kristoffer Dalby <kristoffer@dalby.cc>
2023-12-29 13:55:50 +0100

    tjoda: fix sonos and unifi ips
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/core.tjoda/dnsmasq.nix
+++ machines/core.tjoda/dnsmasq.nix
@@ -14,6 +14,131 @@
       ipAddress = "10.62.0.6";
       mac = "60:12:8b:d2:ee:72";
     }
+
+    # Unifi networking
+    {
+      hostname = "hus-kontor-switch";
+      ipAddress = "10.62.0.110";
+      mac = "b4:fb:e4:24:f9:61";
+    }
+    {
+      hostname = "love-loft-switch";
+      ipAddress = "10.62.0.111";
+      mac = "ac:8b:a9:b0:db:63";
+    }
+    {
+      hostname = "love-kontor-switch";
+      ipAddress = "10.62.0.112";
+      mac = "74:ac:b9:d7:a4:23";
+    }
+    {
+      hostname = "love-scene-switch";
+      ipAddress = "10.62.0.113";
+      mac = "f4:92:bf:a3:6a:59";
+    }
+    {
+      hostname = "bryggerhus-switch";
+      ipAddress = "10.62.0.114";
+      mac = "f4:92:bf:a3:6a:23";
+    }
+    {
+      hostname = "hus-kontor-ap";
+      ipAddress = "10.62.0.120";
+      mac = "74:ac:b9:63:2f:81";
+    }
+    {
+      hostname = "hus-spisestue-ap";
+      ipAddress = "10.62.0.121";
+      mac = "74:ac:b9:c6:50:e7";
+    }
+    {
+      hostname = "love-scene-ap";
+      ipAddress = "10.62.0.122";
+      mac = "fc:ec:da:a6:39:3b";
+    }
+    {
+      hostname = "love-selskap-ap";
+      ipAddress = "10.62.0.123";
+      mac = "04:18:d6:86:72:89";
+    }
+    {
+      hostname = "love-lager-ap";
+      ipAddress = "10.62.0.124";
+      mac = "04:18:d6:86:73:1f";
+    }
+    {
+      hostname = "bryggerhus-ap";
+      ipAddress = "10.62.0.125";
+      mac = "e0:63:da:25:cc:1e";
+    }
+
+    # Sonos
+    {
+      hostname = "hus-kjokken-sonos";
+      ipAddress = "10.62.0.140";
+      mac = "B8:E9:37:AE:5B:E4";
+    }
+    {
+      hostname = "hus-salong-sonos";
+      ipAddress = "10.62.0.141";
+      mac = "B8:E9:37:AE:5B:40";
+    }
+    {
+      hostname = "hus-spisestue-sonos";
+      ipAddress = "10.62.0.142";
+      mac = "B8:E9:37:AE:5A:22";
+    }
+    {
+      hostname = "hus-kontor-sonos";
+      ipAddress = "10.62.0.143";
+      mac = "B8:E9:37:AE:5A:6C";
+    }
+    {
+      hostname = "hus-gang-sonos";
+      ipAddress = "10.62.0.144";
+      mac = "B8:E9:37:AE:5B:C8";
+    }
+    {
+      hostname = "hus-hage-sonos";
+      ipAddress = "10.62.0.145";
+      mac = "B8:E9:37:AE:5B:AC";
+    }
+
+    # Sonos låve
+    {
+      hostname = "love-kontor-bridge-sonos";
+      ipAddress = "192.168.200.140";
+      mac = "b8:e9:37:14:51:14";
+    }
+    {
+      hostname = "love-salong-sonos";
+      ipAddress = "192.168.200.141";
+      mac = "B8:E9:37:91:29:1C";
+    }
+    {
+      hostname = "love-spisestue-sonos";
+      ipAddress = "192.168.200.142";
+      mac = "B8:E9:37:93:0D:24";
+    }
+    {
+      hostname = "love-dansegulv-sonos";
+      ipAddress = "192.168.200.143";
+      mac = "B8:E9:37:91:29:26";
+    }
+
+    # TODO: Love loft sonos?
+    # {
+    #   hostname = "love-dansegulv-sonos";
+    #   ipAddress = "192.168.200.14";
+    #   mac = "B8:E9:37:91:29:26";
+    # }
+
+    # Atlas probe
+    {
+      hostname = "atlas-probe";
+      ipAddress = "192.168.200.101";
+      mac = "02:01:eb:ef:a0:e9";
+    }
   ];
 
   # Allow DNS from selskap
@@ -38,7 +163,7 @@
 
       dhcp-range = [
         "interface:${lan},10.62.0.171,10.62.0.250,255.255.255.0,12h"
-        "interface:${selskap},192.168.200.100,192.168.200.200,255.255.255.0,12h"
+        "interface:${selskap},192.168.200.171,192.168.200.250,255.255.255.0,12h"
       ];
 
       dhcp-option = [
