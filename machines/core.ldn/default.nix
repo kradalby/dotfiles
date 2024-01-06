8493716d6074d8e8fb12b1296964bb78d84acb9c
Kristoffer Dalby <kristoffer@dalby.cc>
2024-01-06 16:33:08 +0100

    ldn: migrate to networkd and nftables
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/core.ldn/default.nix
+++ machines/core.ldn/default.nix
@@ -1,7 +1,5 @@
 {
   config,
-  flakes,
-  pkgs,
   lib,
   ...
 }: {
@@ -24,16 +22,13 @@
     ./dnsmasq.nix
     ./avahi.nix
     ./openvpn.nix
+    ./networking.nix
+    ./nft.nix
   ];
 
   # my.wan = "wlan0";
-  my.wan = "wan";
-  my.lan = "eth0";
-
-  environment.systemPackages = with pkgs; [
-  ];
-
-  services.resolved.enable = false;
+  my.wan = "wan0";
+  my.lan = "lan0";
 
   boot.kernel.sysctl = {
     # if you use ipv4, this is all you need
@@ -53,151 +48,151 @@
     "net.ipv6.conf.${config.my.wan}.autoconf" = 1;
   };
 
-  networking = {
-    hostName = "core";
-    domain = "ldn.fap.no";
-    nameservers = [
-      "1.1.1.1"
-      "1.0.0.1"
-    ];
-    usePredictableInterfaceNames = lib.mkForce true;
+  # networking = {
+  # hostName = "core";
+  # domain = "ldn.fap.no";
+  # nameservers = [
+  #   "1.1.1.1"
+  #   "1.0.0.1"
+  # ];
+  # usePredictableInterfaceNames = lib.mkForce true;
+  #
+  # dhcpcd = {
+  #   enable = true;
+  #   # Do not remove interface configuration on shutdown.
+  #   persistent = true;
+  #   allowInterfaces = [config.my.wan];
+  #   extraConfig = ''
+  #     noipv6rs
+  #     interface ${config.my.wan}
+  #       ipv6rs
+  #       # DHCPv6-PD.
+  #       ia_na 0
+  #       ia_pd 1/::/48 ${config.my.lan}/0/64 iot/156/64
+  #       # IPv4 DHCP ISP settings overrides.
+  #       static domain_name_servers=10.65.0.1
+  #       static domain_search=
+  #       static domain_name=
+  #   '';
+  # };
 
-    dhcpcd = {
-      enable = true;
-      # Do not remove interface configuration on shutdown.
-      persistent = true;
-      allowInterfaces = [config.my.wan];
-      extraConfig = ''
-        noipv6rs
-        interface ${config.my.wan}
-          ipv6rs
-          # DHCPv6-PD.
-          ia_na 0
-          ia_pd 1/::/48 ${config.my.lan}/0/64 iot/156/64
-          # IPv4 DHCP ISP settings overrides.
-          static domain_name_servers=10.65.0.1
-          static domain_search=
-          static domain_name=
-      '';
-    };
+  # wireless = {
+  #   enable = true;
+  #   interfaces = ["wlan0"];
+  #   networks = {
+  #     kPhone15 = {
+  #       # Not really critical since my phone is not stationary and this is not
+  #       # an important password
+  #       pskRaw = "97f4d338b47d77ba29c03305c1e21e3f21e7749d34071fd1fc516fda5a5e8e49";
+  #     };
+  #   };
+  # };
 
-    wireless = {
-      enable = true;
-      interfaces = ["wlan0"];
-      networks = {
-        kPhone15 = {
-          # Not really critical since my phone is not stationary and this is not
-          # an important password
-          pskRaw = "97f4d338b47d77ba29c03305c1e21e3f21e7749d34071fd1fc516fda5a5e8e49";
-        };
-      };
-    };
+  # vlans = {
+  #   wan = {
+  #     interface = config.my.lan;
+  #     id = 3;
+  #   };
+  #
+  #   iot = {
+  #     interface = config.my.lan;
+  #     id = 5;
+  #   };
+  # };
 
-    vlans = {
-      wan = {
-        interface = config.my.lan;
-        id = 3;
-      };
+  # bridges = {
+  #   lan.interfaces = [ "eth0" "wlan0" ];
+  # };
 
-      iot = {
-        interface = config.my.lan;
-        id = 5;
-      };
-    };
+  # defaultGateway = "192.168.2.254";
 
-    # bridges = {
-    #   lan.interfaces = [ "eth0" "wlan0" ];
-    # };
+  # interfaces = {
+  #   wan = {
+  #     useDHCP = true;
+  #     ipv4.addresses = [
+  #       {
+  #         address = "192.168.2.2";
+  #         prefixLength = 24;
+  #       }
+  #     ];
+  #     tempAddress = "disabled";
+  #   };
+  #
+  #   wlan0 = {
+  #     useDHCP = true;
+  #     tempAddress = "disabled";
+  #   };
+  #
+  #   ${config.my.lan} = {
+  #     # It looks like Community Fiber has whitelisted this
+  #     # Mac address somehow, so keep it around incase we
+  #     # need a new machine.
+  #     macAddress = "dc:a6:32:08:d3:e8";
+  #
+  #     useDHCP = false;
+  #     ipv4.addresses = [
+  #       {
+  #         address = "10.65.0.1";
+  #         prefixLength = 24;
+  #       }
+  #     ];
+  #     tempAddress = "disabled";
+  #   };
+  #
+  #   iot = {
+  #     useDHCP = false;
+  #     ipv4.addresses = [
+  #       {
+  #         address = "192.168.156.1";
+  #         prefixLength = 24;
+  #       }
+  #     ];
+  #     tempAddress = "disabled";
+  #   };
+  # };
 
-    defaultGateway = "192.168.2.254";
-
-    interfaces = {
-      wan = {
-        useDHCP = true;
-        ipv4.addresses = [
-          {
-            address = "192.168.2.2";
-            prefixLength = 24;
-          }
-        ];
-        tempAddress = "disabled";
-      };
-
-      wlan0 = {
-        useDHCP = true;
-        tempAddress = "disabled";
-      };
-
-      ${config.my.lan} = {
-        # It looks like Community Fiber has whitelisted this
-        # Mac address somehow, so keep it around incase we
-        # need a new machine.
-        macAddress = "dc:a6:32:08:d3:e8";
-
-        useDHCP = false;
-        ipv4.addresses = [
-          {
-            address = "10.65.0.1";
-            prefixLength = 24;
-          }
-        ];
-        tempAddress = "disabled";
-      };
-
-      iot = {
-        useDHCP = false;
-        ipv4.addresses = [
-          {
-            address = "192.168.156.1";
-            prefixLength = 24;
-          }
-        ];
-        tempAddress = "disabled";
-      };
-    };
-
-    nat = {
-      enable = true;
-      externalInterface = config.my.wan;
-      internalIPs = ["10.0.0.0/8" "192.168.156.0/24"];
-      internalInterfaces = [config.my.lan "iot"];
-      forwardPorts = [
-        {
-          sourcePort = 64322;
-          destination = "10.65.0.1:22";
-          proto = "tcp";
-        }
-        {
-          sourcePort = 500;
-          destination = "10.65.0.1:51820";
-          proto = "udp";
-        }
-        {
-          sourcePort = 4500;
-          destination = "10.65.0.1:51820";
-          proto = "udp";
-        }
-      ];
-    };
-
-    firewall = {
-      # This is a special override for gateway machines as we
-      # dont want to use "openFirewall" here since it makes
-      # everything world available.
-      allowedTCPPorts = lib.mkForce [
-        80 # HTTP
-        443 # HTTPS
-      ];
-
-      allowedUDPPorts = lib.mkForce [
-        443 # HTTPS
-        config.services.tailscale.port
-        # config.networking.wireguard.interfaces.wg0.listenPort
-      ];
-
-      trustedInterfaces = [config.my.lan];
-    };
-  };
+  # nat = {
+  #   enable = true;
+  #   externalInterface = config.my.wan;
+  #   internalIPs = ["10.0.0.0/8" "192.168.156.0/24"];
+  #   internalInterfaces = [config.my.lan "iot"];
+  #   forwardPorts = [
+  #     {
+  #       sourcePort = 64322;
+  #       destination = "10.65.0.1:22";
+  #       proto = "tcp";
+  #     }
+  #     {
+  #       sourcePort = 500;
+  #       destination = "10.65.0.1:51820";
+  #       proto = "udp";
+  #     }
+  #     {
+  #       sourcePort = 4500;
+  #       destination = "10.65.0.1:51820";
+  #       proto = "udp";
+  #     }
+  #   ];
+  # };
+  #
+  # firewall = {
+  #   # This is a special override for gateway machines as we
+  #   # dont want to use "openFirewall" here since it makes
+  #   # everything world available.
+  #   allowedTCPPorts = lib.mkForce [
+  #     80 # HTTP
+  #     443 # HTTPS
+  #   ];
+  #
+  #   allowedUDPPorts = lib.mkForce [
+  #     443 # HTTPS
+  #     config.services.tailscale.port
+  #     # config.networking.wireguard.interfaces.wg0.listenPort
+  #   ];
+  #
+  #   trustedInterfaces = [config.my.lan];
+  # };
+  # };
 
   monitoring.smartctl.devices = ["/dev/sda"];
 
