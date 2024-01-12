25d5bc98f1a3b925cc02407a162842840f141b72
Kristoffer Dalby <kristoffer@dalby.cc>
2024-01-12 20:50:32 +0100

    rpi: bluetooth
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- common/rpi4-configuration.nix
+++ common/rpi4-configuration.nix
@@ -32,6 +32,15 @@
 
   hardware.enableRedistributableFirmware = true;
 
+  systemd.services.btattach = {
+    before = ["bluetooth.service"];
+    after = ["dev-ttyAMA0.device"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.bluez}/bin/btattach -B /dev/ttyAMA0 -P bcm -S 3000000";
+    };
+  };
+
   fileSystems = {
     "/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
