f96e0f5aabc43e31a02f07027cee71ef952af23c
Kristoffer Dalby <kristoffer@dalby.cc>
2024-01-13 09:20:26 +0100

    ragenix: rekey
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

---
+++ machines/eye.ldn/default.nix
@@ -0,0 +1,51 @@
+{
+  config,
+  flakes,
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [
+    ../../common
+    ./hardware-configuration.nix
+
+    ../../common/acme.nix
+    ../../common/nginx.nix
+    ../../common/containers.nix
+
+    ../../common/rpi4-configuration.nix
+
+    # ./tailscale.nix
+    # ./tailscale-headscale.nix
+  ];
+
+  networking = {
+    hostName = "eye";
+    domain = "ldn.fap.no";
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+    usePredictableInterfaceNames = lib.mkForce true;
+    interfaces = {
+      "eth0" = {
+        useDHCP = true;
+      };
+
+      "wlan0" = {
+        useDHCP = true;
+      };
+    };
+
+    wireless = {
+      enable = true;
+      environmentFile = config.age.secrets.ldn-wifi.path;
+      networks = {
+        "_kad".psk = "@PSK_UNDERSCORE_KAD@";
+      };
+    };
+  };
+
+  monitoring.smartctl.devices = ["/dev/sda"];
+  system.stateVersion = "20.09"; # Did you read the comment?
+}
---
+++ machines/eye.ldn/hardware-configuration.nix
@@ -0,0 +1,13 @@
+{
+  pkgs,
+  config,
+  modulesPath,
+  ...
+}: {
+  boot.extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
+  boot.kernelModules = ["v4l2loopback"];
+
+  services.blueman.enable = true;
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+}
