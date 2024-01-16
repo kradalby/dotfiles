5e117f0e924f63e8683d84c2f49a55017f581cfd
Kristoffer Dalby <kristoffer@dalby.cc>
2024-01-16 22:01:46 +0100

    ship: add conditional
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- shipflakes.fish
+++ shipflakes.fish
@@ -3,6 +3,21 @@
 # This script ships this directory to the machines, useful if Colmena isnt behaving
 
 rg "= nixosBox" --no-heading | awk '{print $2}' | rg -v "#" | tr -d '"' | while read host
+    if [ $host != "=" ]
+        # echo "adding host to list: $host"
+        set -a hosts $host
+    end
+end
+
+
+if set -q argv[1]
+    set host $argv[1]
     echo "Shipping to $host..."
-    rsync -avh --progress --delete --cvs-exclude --filter=':- .gitignore' . $host:/etc/nixos/.
+    rsync -ah --delete --cvs-exclude --filter=':- .gitignore' . $host:/etc/nixos/.
+else
+    echo "Shipping flakes to $hosts"
+    for host in $hosts
+        echo "Shipping to $host..."
+        rsync -ah --delete --cvs-exclude --filter=':- .gitignore' . $host:/etc/nixos/.
+    end
 end
