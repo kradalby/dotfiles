3ef084773f19dfb0b3661e1a358226652892b281
Kristoffer Dalby <kristoffer@dalby.cc>
2024-01-16 22:54:21 +0100

    lenovo.ldn: bootstrap2
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/lenovo.ldn/hardware-configuration.nix
+++ machines/lenovo.ldn/hardware-configuration.nix
@@ -5,11 +5,9 @@
   modulesPath,
   ...
 }: {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
-
   boot = {
+    loader.systemd-boot.enable = true;
+
     initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod"];
     initrd.kernelModules = [];
     kernelModules = ["kvm-intel"];
