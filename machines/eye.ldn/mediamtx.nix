ec079cb5583970814c906c57fbe937b21b69c4db
Kristoffer Dalby <kristoffer@dalby.cc>
2024-01-16 22:53:39 +0100

    eye.ldn: make ffmpeg less verbose
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/eye.ldn/mediamtx.nix
+++ machines/eye.ldn/mediamtx.nix
@@ -5,7 +5,7 @@
 }: let
   # this resolution allows two cameras on RPi4
   resolution = "1280x720";
-  c270 = path: "${lib.getExe pkgs.ffmpeg} -f v4l2 -video_size ${resolution} -framerate 5 -i ${path} -pix_fmt yuv420p -c:v libx264 -preset ultrafast -b:v 600k -c:a aac -b:a 160k -f rtsp rtsp://localhost:$RTSP_PORT/$RTSP_PATH";
+  c270 = path: "${lib.getExe pkgs.ffmpeg} -hide_banner -loglevel error -f v4l2 -video_size ${resolution} -framerate 5 -i ${path} -pix_fmt yuv420p -c:v libx264 -preset ultrafast -b:v 600k -c:a aac -b:a 160k -f rtsp rtsp://localhost:$RTSP_PORT/$RTSP_PATH";
 in {
   services.mediamtx = {
     enable = true;
