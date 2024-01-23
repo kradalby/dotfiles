c06ebff16399e181e4d52a718b6d293e35e5e8b3
Kristoffer Dalby <kristoffer@dalby.cc>
2024-01-23 20:51:38 +0100

    nix: add more caches
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- common/nix.nix
+++ common/nix.nix
@@ -18,6 +18,17 @@
   ];
 
   nix = {
+    settings = {
+      substituters = [
+        "https://nix-community.cachix.org?priority=41"
+        "https://numtide.cachix.org?priority=42"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      ];
+    };
+
     package = pkgs.nixFlakes;
     extraOptions = "experimental-features = nix-command flakes";
     # settings = {
