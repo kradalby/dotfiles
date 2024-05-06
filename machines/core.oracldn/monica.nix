44df94aec87024bf43f7f5a4b7e85eed88c156bb
Kristoffer Dalby <kristoffer@dalby.cc>
2024-05-06 09:05:42 +0200

    core.oracldn: add monica, but disable
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

---
+++ machines/core.oracldn/monica.nix
@@ -0,0 +1,35 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  # services.tailscale-proxies.monica = {
+  #   enable = true;
+  #   tailscaleKeyPath = config.age.secrets.tailscale-preauthkey.path;
+  #
+  #   hostname = "monica";
+  #   backendPort = port;
+  # };
+
+  # age.secrets.monica-app-key = {
+  #   file = ../../secrets/monica-app-key.age;
+  #   owner = config.services.monica.user;
+  # };
+
+  services.monica = {
+    enable = false;
+
+    appKeyFile = config.age.secrets.monica-app-key.path;
+    appURL = "http://monica.bee-velociraptor.ts.net";
+
+    database.createLocally = false;
+
+    config = {
+      DB_DATABASE = lib.mkForce "${config.services.monica.dataDir}/db.sqlite";
+      DB_HOST = lib.mkForce "";
+      DB_PORT = lib.mkForce "";
+      DB_USERNAME = lib.mkForce "";
+    };
+  };
+}
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -162,4 +162,7 @@ in
     # or a key that can only be used once.
     "tailscale-preauthkey.age".publicKeys = global;
     "headscale-client-preauthkey.age".publicKeys = global;
+
+    # monica
+    "monica-app-key.age".publicKeys = u ++ [hosts.core-oracldn];
   }
