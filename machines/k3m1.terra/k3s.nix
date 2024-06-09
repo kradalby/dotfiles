eb60003f1e12db6fd0dc4a7a52c548cc14b7fdfb
Kristoffer Dalby <kristoffer@dalby.cc>
2024-06-09 11:39:05 +0200

    machines: clean up dead machines
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/k3m1.terra/k3s.nix
+++
@@ -1,9 +0,0 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: let
-  s = import ../../metadata/sites.nix {inherit lib config;};
-in
-  (import ../../common/funcs/k3s.nix {inherit config pkgs lib;}).server s.sites.terra
