70a875e80d88441293ac8d34212a8e3995338d92
Kristoffer Dalby <kristoffer@dalby.cc>
2024-06-09 11:39:05 +0200

    machines: clean up dead machines
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/core.ntnu/openvpn.nix
+++
@@ -1,9 +0,0 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}: let
-  s = import ../../metadata/sites.nix {inherit lib config;};
-  site = s.sites.ntnu;
-in ((import ../../common/funcs/openvpn.nix {inherit config pkgs lib;}).server site)
