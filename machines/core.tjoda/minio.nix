f04b38ecd47cb20b5251a62b3409533dd04d48a8
Kristoffer Dalby <kristoffer@dalby.cc>
2024-06-24 21:51:27 +0200

    minio: add tjoda to sfiber tailnet
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/core.tjoda/minio.nix
+++ machines/core.tjoda/minio.nix
@@ -7,9 +7,27 @@
     ../../common/minio.nix
   ];
 
-  services.minio = {
-    dataDir = [
-      "/storage/backups/minio"
-    ];
+  age.secrets.headscale-sfiber-authkey = {
+    file = ../../secrets/headscale-sfiber-client-preauthkey.age;
+    owner = config.users.users.tailscale-proxy.name;
+  };
+
+  services = {
+    minio = {
+      dataDir = [
+        "/storage/backup/minio"
+      ];
+    };
+    tailscale-proxies = {
+      minio-sfiber = {
+        enable = true;
+        tailscaleKeyPath = config.age.secrets.headscale-sfiber-authkey.path;
+        loginServer = "https://headscale.sandefjordfiber.no";
+
+        hostname = "minio-tjoda";
+        # TODO(kradalby): replace with services.minio.listenAddress
+        backendPort = 9000;
+      };
+    };
   };
 }
