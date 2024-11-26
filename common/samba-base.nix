32088d3d7a4c115d9badecb571aa2a108d2515c6
Kristoffer Dalby <kristoffer@dalby.cc>
2024-11-26 18:18:19 +0100

    samba: move to settings
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- common/samba-base.nix
+++ common/samba-base.nix
@@ -9,36 +9,34 @@ in {
     # REMIND: `smbpasswd -a`
 
     enable = true;
+    settings = {
+      global = {
+        "netbios name" = "${config.networking.hostName}-${site}";
+        "mdns name" = "mdns";
+        "server string" = "Samba on ${config.networking.hostName}.${site}";
 
-    extraConfig = ''
-      netbios name = ${config.networking.hostName}-${site}
-      mdns name = mdns
-      server string = Samba on ${config.networking.hostName}.${site}
+        # This would preferrably be SMB3, but iOS Files.app does not
+        # support authing/handshaking over SMB3.
+        "min protocol" = "SMB2";
+        "encrypt passwords" = true;
 
-      # This would preferrably be SMB3, but iOS Files.app does not
-      # support authing/handshaking over SMB3.
-      min protocol = SMB2
-      encrypt passwords = true
+        "wins support" = true;
+        "local master" = true;
+        "preferred master" = true;
+        workgroup = "WORKGROUP";
 
-      # Windows discovery
-      wins support = yes
-      local master = yes
-      preferred master = yes
-      workgroup = WORKGROUP
+        "vfs objects" = "catia fruit streams_xattr";
 
-      vfs objects = catia fruit streams_xattr
-
-      # macOS optimalisation
-      fruit:aapl = yes
-      fruit:metadata = stream
-      fruit:model = MacPro7,1
-      fruit:posix_rename = yes
-      fruit:veto_appledouble = no
-      fruit:wipe_intentionally_left_blank_rfork = yes
-      fruit:delete_empty_adfiles = yes
-      fruit:advertise_fullsync = true
-
-      map to guest = bad user
-    '';
+        # macOS optimalisation
+        "fruit:aapl" = true;
+        "fruit:metadata" = "stream";
+        "fruit:model" = "MacPro7,1";
+        "fruit:posix_rename" = true;
+        "fruit:veto_appledouble" = false;
+        "fruit:wipe_intentionally_left_blank_rfork" = true;
+        "fruit:delete_empty_adfiles" = true;
+        "fruit:advertise_fullsync" = true;
+      };
+    };
   };
 }
