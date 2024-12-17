35c71a173934567bf5677e0855827ca60ec5b522
Kristoffer Dalby <kristoffer@tailscale.com>
2024-12-17 20:00:21 +0100

    restic: tailscale init auth
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- machines/core.terra/rest-server.nix
+++ machines/core.terra/rest-server.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   lib,
   config,
   ...
@@ -17,6 +18,40 @@ in
         prometheus = true;
         listenAddress = "${toString port}";
         extraFlags = ["--no-auth"];
+      };
+
+      users.users.tailscale-restic-proxy = {
+        isSystemUser = true;
+        home = "/var/lib/tailscale-restic-proxy";
+        description = "Tailscale Restic Proxy";
+        createHome = true;
+        shell = "/run/current-system/sw/bin/nologin";
+        group = "tailscale-restic-proxy";
+      };
+      users.groups.tailscale-restic-proxy = {};
+
+      systemd.services.tailscale-restic-proxy = {
+        enable = true;
+        description = "Tailscale Restic Proxy";
+        wantedBy = ["multi-user.target"];
+        after = ["restic-rest-server.service"];
+
+        script = ''
+            export TS_AUTHKEY=$(cat ${config.age.secrets.tailscale-preauthkey.path})
+          ${pkgs.tailscale-restic-proxy}/bin/ts-restic-proxy \
+            --ts-auth-key $TS_AUTHKEY \
+            --restic-rest-server http://127.0.0.1:${toString port}/ \
+            --hostname=restic-terra \
+            --htpasswd-file ${config.users.users.tailscale-restic-proxy.home}/htpasswd
+        '';
+
+        serviceConfig = {
+          User = "tailscale-restic-proxy";
+          Group = "tailscale-restic-proxy";
+          Restart = "always";
+          RestartSec = "15";
+          WorkingDirectory = config.users.users.tailscale-restic-proxy.home;
+        };
       };
 
       my.consulServices.restic_server = consul.prometheusExporter "rest-server" port;
--- machines/core.tjoda/rest-server.nix
+++ machines/core.tjoda/rest-server.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   lib,
   config,
   ...
