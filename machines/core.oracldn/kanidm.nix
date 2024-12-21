802a4171a8a41a09a612205533194122522df936
Kristoffer Dalby <kristoffer@tailscale.com>
2024-12-21 10:22:11 +0100

    kanidm: setup with provision and headscale
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- machines/core.oracldn/kanidm.nix
+++ machines/core.oracldn/kanidm.nix
@@ -1,20 +1,76 @@
-{config, ...}: let
+{
+  config,
+  pkgs,
+  ...
+}: let
   domain = "auth.kradalby.no";
+  certDir = config.security.acme.certs."${domain}".directory;
 in {
-  services.kanidm.enableServer = true;
-  services.kanidm.serverSettings = {
-    inherit domain;
-    tls_chain = "/var/lib/acme/${domain}/fullchain.pem";
-    tls_key = "/var/lib/acme/${domain}/key.pem";
-    bindaddress = "[::1]:3013";
-    ldapbindaddress = "[::1]:3636";
-    origin = "https://${domain}";
-    trust_x_forward_for = true;
-    db_fs_type = "xfs";
+  age.secrets = {
+    kanidm-admin-password = {
+      file = ../../secrets/kanidm-admin-password.age;
+      owner = "kanidm";
+      group = "kanidm";
+    };
 
-    online_backup = {
-      path = "/var/lib/kanidm/backup";
-      schedule = "0 0 * * *";
+    kanidm-idm-admin-password = {
+      file = ../../secrets/kanidm-idm-admin-password.age;
+      owner = "kanidm";
+      group = "kanidm";
+    };
+
+    kanidm-headscale-oidc-secret = {
+      file = ../../secrets/headscale-oidc-secret.age;
+      owner = "kanidm";
+      group = "kanidm";
+    };
+  };
+
+  services.kanidm = {
+    package = pkgs.kanidm.withSecretProvisioning;
+    enableServer = true;
+    serverSettings = {
+      inherit domain;
+      origin = "https://${domain}";
+
+      tls_key = "${certDir}/key.pem";
+      tls_chain = "${certDir}/full.pem";
+
+      bindaddress = "127.0.0.1:3013";
+      ldapbindaddress = "127.0.0.1:3636";
+      trust_x_forward_for = true;
+      db_fs_type = "xfs";
+
+      online_backup = {
+        path = "/var/lib/kanidm/backup";
+        schedule = "0 0 * * *";
+      };
+    };
+
+    provision = {
+      enable = true;
+      adminPasswordFile = config.age.secrets.kanidm-admin-password.path;
+      idmAdminPasswordFile = config.age.secrets.kanidm-idm-admin-password.path;
+
+      persons = {
+        kradalby = {
+          displayName = "Kristoffer Dalby";
+          mailAddresses = ["kristoffer@dalby.cc"];
+          groups = [];
+        };
+      };
+
+      groups = {
+        vpn_users.members = ["kradalby"];
+      };
+
+      systems.oauth2.headscale = {
+        originUrl = "https://headscale.kradalby.no/oidc/callback";
+        originLanding = "https://headscale.kradalby.no";
+        displayName = "Headscale";
+        scopeMaps.vpn_users = ["openid" "profile" "email"];
+        basicSecretFile = config.age.secrets.kanidm-headscale-oidc-secret.path;
+      };
     };
   };
 
@@ -31,7 +87,7 @@ in {
     useACMEHost = domain;
     forceSSL = true;
     locations."/" = {
-      proxyPass = "https://[::1]:3013";
+      proxyPass = "https://127.0.0.1:3013";
     };
   };
 }
