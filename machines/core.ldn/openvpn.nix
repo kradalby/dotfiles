0feb0a569be7bc0f949a521841f5d0c3145961f8
Kristoffer Dalby <kristoffer@tailscale.com>
2024-12-22 21:22:11 +0100

    core.ldn: remove config
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- machines/core.ldn/openvpn.nix
+++
@@ -1,9 +0,0 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}: let
-  s = import ../../metadata/sites.nix {inherit lib config;};
-  site = s.sites.ldn;
-in ((import ../../common/funcs/openvpn.nix {inherit config pkgs lib;}).server site)
