61a4c62e1dac07adcf40ae6dd9449af9b48f8ea5
Kristoffer Dalby <kristoffer@tailscale.com>
2025-01-07 08:58:11 +0100

    pkgs: reuntroduce pamfix since reattach isnt supported by nixdarwin
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- pkgs/workstation.nix
+++ pkgs/workstation.nix
@@ -70,6 +70,21 @@ in {
       qemu
 
       sql-studio-mac
+
+      (pkgs.writeScriptBin
+        "pamtouchfix"
+        ''
+          #!/run/current-system/sw/bin/bash
+          cat <<EOT > /etc/pam.d/sudo
+          auth       optional       /opt/homebrew/lib/pam/pam_reattach.so
+          auth       sufficient     pam_tid.so
+          auth       sufficient     pam_smartcard.so
+          auth       required       pam_opendirectory.so
+          account    required       pam_permit.so
+          password   required       pam_deny.so
+          session    required       pam_permit.so
+          EOT
+        '')
     ]
     ++ lib.optionals stdenv.isLinux [
       # swift
