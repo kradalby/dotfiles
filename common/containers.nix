12602afccfea16a6c84b47d2ef5c16ed93cf36f3
Kristoffer Dalby <kristoffer@tailscale.com>
2025-02-07 19:34:29 +0100

    containers: clean over time
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- common/containers.nix
+++ common/containers.nix
@@ -1,4 +1,11 @@
 {pkgs, ...}: {
   virtualisation.oci-containers.backend = "docker";
-  virtualisation.docker.package = pkgs.docker;
+  virtualisation.docker = {
+    package = pkgs.docker;
+    enable = true;
+    autoPrune = {
+      enable = true;
+      flags = ["--all"];
+    };
+  };
 }
