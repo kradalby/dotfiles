39149f1b0b618a9e02485a284a65a72f857bbed6
Kristoffer Dalby <kristoffer@tailscale.com>
2025-07-09 17:53:41 +0200

    common: remove my own cache for now
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- common/nix.nix
+++ common/nix.nix
@@ -21,12 +21,10 @@
       substituters = [
         "https://nix-community.cachix.org?priority=41"
         "https://numtide.cachix.org?priority=42"
-        # "http://attic.dalby.ts.net/system?priority=43"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
-        # "system:40arGOg81ZACFJQAksoEplo8PfgxDd6aEQpNbuHXcCg="
       ];
     };
 
