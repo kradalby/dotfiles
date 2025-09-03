059ad6f36d7a71a0970dffdb6770d96f3ed28da9
Kristoffer Dalby <kristoffer@tailscale.com>
2025-09-03 09:32:32 +0200

    finicky: more asterix
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- rc/finicky.js
+++ rc/finicky.js
@@ -36,8 +36,8 @@ module.exports = {
         "*.discordapp.com",
         "discord.com",
         "*.discord.com",
-        "discordcdn.com"
-        "*.discordcdn.com"
+        "discordcdn.com",
+        "*.discordcdn.com",
       ]),
       browser: "Safari"
     },
