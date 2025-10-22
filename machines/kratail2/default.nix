b4e167c7bf52c2de9d7a0c61c76536b7c3c5a43b
Kristoffer Dalby <kristoffer@tailscale.com>
2025-10-22 16:24:34 +0200

    darwin: DRY up macs
    Signed-off-by: Kristoffer Dalby <kristoffer@tailscale.com>

--- machines/kratail2/default.nix
+++ machines/kratail2/default.nix
@@ -1,65 +1,24 @@
+# Work Mac configuration (Tailscale)
 {
-  pkgs,
-  config,
-  machine,
   lib,
-  stdenv,
   ...
 }: {
   imports = [
-    ../../common/darwin.nix
-
-    ../../pkgs/system.nix
-    ../../pkgs/homebrew.nix
-    ./syncthing.nix
+    ../../common/darwin/kradalby-base.nix
   ];
 
-  nix = {
-    settings = {
-      trusted-users = [machine.username];
-      builders = "@/etc/nix/machines";
+  # Work-specific overrides
+  home-manager.users.kradalby = {
+    programs.git = {
+      userEmail = lib.mkForce "kristoffer@tailscale.com";
     };
 
-    distributedBuilds = true;
-    buildMachines = import ../../common/buildmachines.nix;
-  };
-
-  users.users.kradalby = {
-    name = machine.username;
-    home = machine.homeDir;
-  };
-
-  home-manager = {
-    verbose = true;
-    backupFileExtension = "hm_bak~";
-    useUserPackages = true;
-    useGlobalPkgs = true;
-    users."${machine.username}" = {
-      imports = [
-        ../../home
-        # ./scripts.nix
-      ];
-
-      programs.git = {
-        userEmail = lib.mkForce "kristoffer@tailscale.com";
-      };
-
-      home.sessionVariables = {
-        TS_NIX_SHELL_XCODE_VERSION = "15.4";
-        TS_NIX_SHELL_XCODE_WRAPPER_DISABLED = "1";
-      };
+    home.sessionVariables = {
+      TS_NIX_SHELL_XCODE_VERSION = "15.4";
+      TS_NIX_SHELL_XCODE_WRAPPER_DISABLED = "1";
     };
   };
 
-  security.pam.services.sudo_local.touchIdAuth = true;
-
-  networking = {
-    hostName = machine.hostname;
-    computerName = machine.hostname;
-    localHostName = machine.hostname;
-  };
-
-  # Work stuff
   homebrew = {
     casks = [
       "google-chrome"
@@ -72,17 +31,5 @@
       "slack-cli"
       "logi-options+"
     ];
-  };
-
-  system = {
-    primaryUser = "kradalby";
-    defaults = {
-      smb.NetBIOSName = machine.hostname;
-      dock.orientation = lib.mkForce "left";
-    };
-
-    # Used for backwards compatibility, please read the changelog before changing.
-    # $ darwin-rebuild changelog
-    stateVersion = 5;
   };
 }
