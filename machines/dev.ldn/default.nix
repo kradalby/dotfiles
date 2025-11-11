8b09ae24cc0e6d0d9bf7ec1d6c33f0f98cc73163
Kristoffer Dalby <kristoffer@tailscale.com>
2025-11-11 04:00:31 +0100

    dev.ldn: use unstable lima to avoid CVEs
    Use pkgs.unstable.lima (v1.2.2) instead of pkgs.lima (v1.0.7)
    to avoid CVE-2024-25621, CVE-2025-64329, CVE-2025-31133,
    CVE-2025-52565, and CVE-2025-52881.
    
    Reference: https://github.com/NixOS/nixpkgs/pull/460087

--- machines/dev.ldn/default.nix
+++ machines/dev.ldn/default.nix
@@ -84,7 +84,7 @@ in {
     # Do install the docker CLI to talk to podman.
     # Not needed when virtualisation.docker.enable = true;
     pkgs.docker-client
-    pkgs.lima
+    pkgs.unstable.lima
     pkgs.nodejs_24
   ];
 
