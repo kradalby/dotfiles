708035c09a948a85aadb0ebc66011ce9b0534e6a
Kristoffer Dalby <kristoffer@dalby.cc>
2025-11-23 22:29:40 +0100

    syncthing: replace nginx internal with tailscale services
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- common/syncthing-storage.nix
+++ common/syncthing-storage.nix
@@ -3,67 +3,69 @@
   lib,
   ...
 }: let
-  nginx = import ./funcs/nginx.nix {inherit config lib;};
-
   cfg = import ../metadata/syncthing.nix;
-  domain = "syncthing.core.${config.networking.domain}";
-in
-  lib.mkMerge [
-    {
-      services = {
-        syncthing = {
-          user = "storage";
-          group = "storage";
-          dataDir = "/storage";
-          enable = true;
-          overrideDevices = true;
-          overrideFolders = true;
-          settings = {
-            inherit (cfg) devices;
-            gui.insecureSkipHostcheck = true;
-            folders = {
-              "/storage/software" = {
-                id = "vpgyn-cj2mg";
-                path = "/storage/software";
-                devices = cfg.storage;
-                type = "sendreceive";
-              };
+  location =
+    let
+      components = lib.splitString "." config.networking.domain;
+    in
+      lib.elemAt components 0;
+  tailscaleService = "svc:syncthing-${location}";
+in {
+  services = {
+    syncthing = {
+      user = "storage";
+      group = "storage";
+      dataDir = "/storage";
+      enable = true;
+      overrideDevices = true;
+      overrideFolders = true;
+      settings = {
+        inherit (cfg) devices;
+        gui.insecureSkipHostcheck = true;
+        folders = {
+          "/storage/software" = {
+            id = "vpgyn-cj2mg";
+            path = "/storage/software";
+            devices = cfg.storage;
+            type = "sendreceive";
+          };
 
-              "/storage/pictures" = {
-                id = "orqnv-bg72d";
-                path = "/storage/pictures";
-                devices = cfg.storage;
-                type = "sendreceive";
-              };
+          "/storage/pictures" = {
+            id = "orqnv-bg72d";
+            path = "/storage/pictures";
+            devices = cfg.storage;
+            type = "sendreceive";
+          };
 
-              "/storage/backup" = {
-                id = "9bjac-k65uu";
-                path = "/storage/backup";
-                devices = cfg.storage;
-                type = "sendreceive";
-              };
+          "/storage/backup" = {
+            id = "9bjac-k65uu";
+            path = "/storage/backup";
+            devices = cfg.storage;
+            type = "sendreceive";
+          };
 
-              "/storage/books" = {
-                id = "ww4gn-xgy9i";
-                path = "/storage/books";
-                devices = cfg.storage;
-                type = "sendreceive";
-              };
+          "/storage/books" = {
+            id = "ww4gn-xgy9i";
+            path = "/storage/books";
+            devices = cfg.storage;
+            type = "sendreceive";
+          };
 
-              "kradalby - Sync" = {
-                id = "xTDuT-kZeuK";
-                path = "/storage/sync/kradalby";
-                devices = builtins.attrNames config.services.syncthing.settings.devices;
-                type = "sendreceive";
-              };
-            };
+          "kradalby - Sync" = {
+            id = "xTDuT-kZeuK";
+            path = "/storage/sync/kradalby";
+            devices = builtins.attrNames config.services.syncthing.settings.devices;
+            type = "sendreceive";
           };
         };
       };
-    }
+    };
 
-    (nginx.internalVhost {
-      inherit domain;
-      proxyPass = "http://127.0.0.1:8384";
-    })
-  ]
+    tailscale.services.${tailscaleService} = {
+      endpoints = {
+        "tcp:80" = "http://127.0.0.1:8384";
+        "tcp:443" = "http://127.0.0.1:8384";
+      };
+    };
+  };
+}
