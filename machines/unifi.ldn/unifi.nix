8e4a613449335ce26c6933c18a7c46f7eb1b4523
Kristoffer Dalby <kristoffer@dalby.cc>
2025-12-03 17:22:02 +0100

    unifi: migrate mongo to 8 ce
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- machines/unifi.ldn/unifi.nix
+++ machines/unifi.ldn/unifi.nix
@@ -11,7 +11,7 @@
 
       services.unifi = {
         unifiPackage = pkgs.unifi;
-        mongodbPackage = pkgs.mongodb;
+        mongodbPackage = pkgs.mongodb-ce;
 
         enable = true;
         openFirewall = true;
