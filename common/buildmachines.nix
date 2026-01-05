04c7b51248128e514b654d4c320204e134849c8d
Kristoffer Dalby <kristoffer@dalby.cc>
2026-01-05 18:32:14 +0100

    common: disable build machines
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

--- common/buildmachines.nix
+++ common/buildmachines.nix
@@ -3,37 +3,37 @@
 # adding the base64 key ourselves:
 # base64 -w0 /etc/ssh/ssh_host_ed25519_key.pub
 [
-  {
-    hostName = "dev.oracfurt.fap.no";
-    systems = ["aarch64-linux"];
-    sshUser = "root";
-    sshKey = "/Users/kradalby/.ssh/id_ed25519";
-    maxJobs = 3;
-    supportedFeatures = ["big-parallel" "kvm" "nixos-test"];
-    publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUU2NXMvaFJuMzR2NVVOaFNJQzgvSk4vNDUyaExkcW4xMzFnVnFxQlRQbmwgcm9vdEBkZXYK";
-  }
-  {
-    hostName = "dev.ldn.fap.no";
-    systems = [
-      "x86_64-linux"
-      # "aarch64-linux"
-    ];
-    sshUser = "root";
-    sshKey = "/Users/kradalby/.ssh/id_ed25519";
-    maxJobs = 3;
-    supportedFeatures = ["big-parallel" "kvm" "nixos-test"];
-    publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUhOclJpZVZmckN2bnFOYnV4RXIwNmM2RDEvbGhHbEVJdlM4Tk5RaHJtSnQ=";
-  }
-  {
-    hostName = "dev-ldn.dalby.ts.net";
-    systems = [
-      "x86_64-linux"
-      # "aarch64-linux"
-    ];
-    sshUser = "root";
-    sshKey = "/Users/kradalby/.ssh/id_ed25519";
-    maxJobs = 3;
-    supportedFeatures = ["big-parallel" "kvm" "nixos-test"];
-    publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUhOclJpZVZmckN2bnFOYnV4RXIwNmM2RDEvbGhHbEVJdlM4Tk5RaHJtSnQ=";
-  }
+  # {
+  #   hostName = "dev.oracfurt.fap.no";
+  #   systems = ["aarch64-linux"];
+  #   sshUser = "root";
+  #   sshKey = "/Users/kradalby/.ssh/id_ed25519";
+  #   maxJobs = 3;
+  #   supportedFeatures = ["big-parallel" "kvm" "nixos-test"];
+  #   publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUU2NXMvaFJuMzR2NVVOaFNJQzgvSk4vNDUyaExkcW4xMzFnVnFxQlRQbmwgcm9vdEBkZXYK";
+  # }
+  # {
+  #   hostName = "dev.ldn.fap.no";
+  #   systems = [
+  #     "x86_64-linux"
+  #     # "aarch64-linux"
+  #   ];
+  #   sshUser = "root";
+  #   sshKey = "/Users/kradalby/.ssh/id_ed25519";
+  #   maxJobs = 3;
+  #   supportedFeatures = ["big-parallel" "kvm" "nixos-test"];
+  #   publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUhOclJpZVZmckN2bnFOYnV4RXIwNmM2RDEvbGhHbEVJdlM4Tk5RaHJtSnQ=";
+  # }
+  # {
+  #   hostName = "dev-ldn.dalby.ts.net";
+  #   systems = [
+  #     "x86_64-linux"
+  #     # "aarch64-linux"
+  #   ];
+  #   sshUser = "root";
+  #   sshKey = "/Users/kradalby/.ssh/id_ed25519";
+  #   maxJobs = 3;
+  #   supportedFeatures = ["big-parallel" "kvm" "nixos-test"];
+  #   publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUhOclJpZVZmckN2bnFOYnV4RXIwNmM2RDEvbGhHbEVJdlM4Tk5RaHJtSnQ=";
+  # }
 ]
