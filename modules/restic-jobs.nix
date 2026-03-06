3af971cbeeb8c378a88fd1c039685500c97acd10
Kristoffer Dalby <kristoffer@dalby.cc>
2026-03-06 17:51:54 +0000

    restic: switch backup URLs to HTTPS
    Now that Tailscale Services terminates TLS natively, restic rest-server
    endpoints are reachable over HTTPS. Update the URL template accordingly.

--- modules/restic-jobs.nix
+++ modules/restic-jobs.nix
@@ -133,9 +133,7 @@ let
         if jobCfg.repository != null
         then jobCfg.repository
         else if jobCfg.site != null
-        # TODO(kradalbyy): Change back to https when services supports terminating
-        # and proxying http.
-        then "rest:http://restic-${jobCfg.site}.dalby.ts.net/${targetHost}"
+        then "rest:https://restic-${jobCfg.site}.dalby.ts.net/${targetHost}"
         else null;
 
       darwinExtras =
