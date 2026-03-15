37ea1776aad472f16a0ee0487df3fb71c364183a
Kristoffer Dalby <kristoffer@dalby.cc>
2026-03-15 18:53:12 +0100

    bin: remove obsolete scripts
    Remove 25 scripts that are no longer functional or relevant:
    - X11/xrandr display scripts (dual, single, mirror, mirror-safe)
    - NTNU/university scripts (sprett, kontoret, rattata)
    - i3 window manager (i3-new-workspace)
    - Emacs launcher (em)
    - Maven Java executor (mvnexec)
    - X11 trackpad/synaptics (trackpad.sh)
    - Manual font installer (adobe-source-code-pro-install)
    - Screenshot upload to stale hosts (ss.sh, ss_linux.sh)
    - Stale SMB mount (samba_onyx)
    - vCenter cert import (import-vcrootcertificate.sh)
    - Perl unbound ad-blocker (unbound-block-hosts)
    - iTerm2-specific display scripts (divider, imgcat, imgls)
    - Kitty terminfo pusher (sshterminfo)
    - Broken GitHub-Gitea mirror (mirror-github-gitea.py)
    - Unused utilities (pdfconcat, update_kubernetes_go.sh, yaml2nix.fish)

--- bin/yaml2nix.fish
+++
@@ -1,4 +0,0 @@
-set json (remarshal -if yaml -of json -i $argv[1])
-echo "builtins.fromJSON ''$json''" \
-   | nix-instantiate --eval --strict -E - \
-   | perl -ne 's/(?!\ )([A-Za-z0-9\-\/]+[\.\/]+[A-Za-z0-9\-\/]+)(?=\ =\ )/"$1"/g; print;'
